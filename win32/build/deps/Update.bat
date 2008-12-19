270907a1bf487388f2aa9a2bfed32fad49da53c9
Marcel Wysocki <maci@satgnu.net>
2008-12-19 14:53:11 +0000

    build routine fix

--- win32/build/deps/Update.bat
+++ win32/build/deps/Update.bat
@@ -1,8 +1,8 @@
 @echo off
 echo This program will update Quake2World.
 echo Please be patient, this may take a while.
-rsync.exe -az --progress --exclude=rsync.exe --exclude=cygwin1.dll rsync://satgnu.net/quake2world-win32 .
-rsync.exe --delete -az --progress rsync://satgnu.net/quake2world temp
+rsync.exe -rz --progress --exclude=rsync.exe --exclude=cygwin1.dll rsync://satgnu.net/quake2world-win32 .
+rsync.exe --delete -rz --progress rsync://satgnu.net/quake2world temp
 xcopy temp\default default /E /Y >nul
 echo Update complete!
 echo If this was the first time you run Update.bat the next update should be a lot faster. ;)
