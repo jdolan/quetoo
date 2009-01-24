7b2812c328eb65902f2be800b751178dabef2ce2
Jay Dolan <jay@jaydolan.com>
2009-01-24 21:12:44 +0000

    No, really fix 64 bit.

--- src/pmove.c
+++ src/pmove.c
@@ -571,7 +571,7 @@ static void Pm_CategorizePosition(void){
 		}
 
 		// hitting the world means we're done being pushed
-		if((int)((char *)trace.ent) == 1)
+		if((int)((long)trace.ent) == 1)
 			pm->s.pm_flags &= ~PMF_PUSHED;
 	}
 
