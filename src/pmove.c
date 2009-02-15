631a946c12903428fc1f4cf3839c4ad3429288a5
Jay Dolan <jay@jaydolan.com>
2009-02-15 22:13:13 +0000

    Re-add second circular clamp present in Quake2 pmove.  This hopefully fixes the rare yet very annoying 'horizontal hosed' bug.

--- src/pmove.c
+++ src/pmove.c
@@ -903,7 +903,7 @@ static void Pm_ClampAngles(void){
 
 	if(pm->s.pm_flags & PMF_TIME_TELEPORT){
 		pm->angles[YAW] = SHORT2ANGLE(pm->cmd.angles[YAW] + pm->s.delta_angles[YAW]);
-		pm->angles[PITCH] = pm->angles[ROLL] = 0.0;
+		pm->angles[PITCH] = pm->angles[ROLL] = 0;
 	} else {
 		// circularly clamp the angles with deltas
 		for(i = 0; i < 3; i++){
@@ -911,15 +911,18 @@ static void Pm_ClampAngles(void){
 		}
 
 		// don't let the player look up or down more than 90 degrees
-		if(pm->angles[PITCH] > 89.0 && pm->angles[PITCH] < 180.0)
-			pm->angles[PITCH] = 89.0;
+		if(pm->angles[PITCH] > 89 && pm->angles[PITCH] < 180)
+			pm->angles[PITCH] = 89;
 
-		else if(pm->angles[PITCH] < 271.0 && pm->angles[PITCH] >= 180.0)
-			pm->angles[PITCH] = 271.0;
+		else if(pm->angles[PITCH] < 271 && pm->angles[PITCH] >= 180)
+			pm->angles[PITCH] = 271;
 	}
 
 	// update the local angles responsible for velocity calculations
 	VectorCopy(pm->angles, angles);
+
+	if(angles[PITCH] > 180.0)  // circular clamp
+		angles[PITCH] -= 360.0;
 
 	// for ground movement, reduce pitch to keep the player moving forward
 	if(pm->s.pm_flags & PMF_ON_GROUND)
