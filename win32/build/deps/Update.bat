1fd120521aa2eb7ca4ab0ea5ad4baeafbb175709
Marcel Wysocki <maci@satgnu.net>
2009-04-25 08:11:02 +0000

    rm ddls

--- win32/build/deps/Update.bat
+++ win32/build/deps/Update.bat
@@ -5,6 +5,5 @@ rsync.exe -rz --progress --exclude=rsync.exe --exclude=cygwin1.dll rsync://jdola
 rsync.exe --delete -rz --progress rsync://jdolan.dyndns.org/quake2world temp
 xcopy temp\default default /E /Y >nul
 echo Update complete!
-echo If this was the first time you run Update.bat the next update should be a lot faster. ;)
 echo Make sure to run this file on a regular basis!
 pause
