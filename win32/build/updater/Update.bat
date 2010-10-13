3d3f3f9b098b61a314efb6390d04dff5f77aface
Stijn Buys <ingar@telenet.be>
2010-10-13 11:45:15 +0000

    Solve the rsync file permissions problem on Vista/Win 7, thanks void-995

--- win32/build/updater/Update.bat
+++ win32/build/updater/Update.bat
@@ -1,6 +1,10 @@
 @echo off
 echo This program will update the Quake2World data files.
 echo Please be patient, this may take a while.
+
+rem This solves the rsync file permissions problem on Vista/Windows 7
+set CYGWIN=nontsec
+
 rsync.exe -rz --progress --exclude=rsync.exe --exclude=cygwin1.dll rsync://jdolan.dyndns.org/quake2world-win32 .
 rsync.exe --delete -rz --progress rsync://jdolan.dyndns.org/quake2world temp
 xcopy temp\default default /E /Y >nul
