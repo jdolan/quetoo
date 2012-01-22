f21ee79fef4d28c4f77c31542889456e784bc2f3
Marcel Wysocki <maci@satgnu.net>
2012-01-22 07:39:51 +0000

    meeeh

--- win32/mingw-install/scripts/10_sdl-mixer.sh
+++ win32/mingw-install/scripts/10_sdl-mixer.sh
@@ -1,10 +1,11 @@
-#compile sdl_mixer #TODO: newer versions fail to build bcs of Makefile bug
 SOURCE=http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.tar.gz
 
 wget -c $SOURCE
 
 tar xzf SDL_mixer-1.2.12.tar.gz
 cd SDL_mixer-1.2.12
+# workaround for libtool linking order fuckup, just dont build playwave and playmus
+sed -i 's#$(objects)/playwave$(EXE) $(objects)/playmus$(EXE)##g' Makefile.in
 ./configure --prefix=/mingw
 make -j 4
 make install
