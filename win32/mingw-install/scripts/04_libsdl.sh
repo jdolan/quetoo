6cbf6c812565a002d3e458ac21aecf465915d9cd
Marcel Wysocki <maci@satgnu.net>
2012-01-24 06:57:02 +0000

    ok, now .15 should work

--- win32/mingw-install/scripts/04_libsdl.sh
+++ win32/mingw-install/scripts/04_libsdl.sh
@@ -1,10 +1,12 @@
-SOURCE=http://www.libsdl.org/release/SDL-1.2.14.tar.gz
+SOURCE=http://www.libsdl.org/release/SDL-1.2.15.tar.gz
 wget -c $SOURCE
 
 
-tar xzf SDL-1.2.14.tar.gz
-cd SDL-1.2.14
+tar xzf SDL-1.2.15.tar.gz
+cd SDL-1.2.15
 
 ./configure --prefix=/mingw
 make -j 4
 make install
+#evil workaround
+rm /mingw/lib/libSDLmain.la
