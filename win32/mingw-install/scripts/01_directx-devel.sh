edc8d4033cc3b5dab603466e22ae664a76b5f8c7
Marcel Wysocki <maci@satgnu.net>
2012-01-25 12:07:22 +0000

    devenv stuff

--- win32/mingw-install/scripts/01_directx-devel.sh
+++ win32/mingw-install/scripts/01_directx-devel.sh
@@ -3,3 +3,17 @@ SOURCE=http://www.libsdl.org/extras/win32/common/directx-devel.tar.gz
 wget -c $SOURCE
 
 tar xzvf directx-devel.tar.gz -C /mingw
+
+
+
+## maybe use newer dx sdk later
+#SOURCE=http://alleg.sourceforge.net/files/dx70_mgw.zip
+
+#rm -Rf dx70_mgw
+#mkdir dx70_mgw
+#cd dx70_mgw
+#wget -c $SOURCE
+#unzip dx70_mgw.zip
+
+#cp -f lib/* /mingw/lib
+#cp -f include/* /mingw/include
