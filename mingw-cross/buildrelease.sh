c6d3bf6856ec73e094472530e8cff09a3076c5eb
Marcel Wysocki <maci@satgnu.net>
2013-06-06 16:31:27 +0200

    win32 release: rsync zipfile to q2w website, add run.bat for now

--- mingw-cross/buildrelease.sh
+++ mingw-cross/buildrelease.sh
@@ -33,8 +33,9 @@ find /tmp/quake2world-${MINGW_TARGET} -name "*.dll.a" -delete
 cp -r ${MINGW_ARCH}/bin /tmp/quake2world-${MINGW_TARGET}
 
 cd /tmp/quake2world-${MINGW_TARGET}
-rsync -avzP bin lib maci@quake2world.net:/opt/rsync/quake2world-win32/${MINGW_ARCH}
+rsync -avzP *.bat bin lib maci@quake2world.net:/opt/rsync/quake2world-win32/${MINGW_ARCH}
 
 mkdir quake2world
 mv bin lib quake2world
 zip -9 -r quake2world-BETA-${MINGW_ARCH}.zip quake2world
+rsync -avzP quake2world-BETA-${MINGW_ARCH}.zip maci@quake2world.net:/var/www/quake2world/files/quake2world-BETA-${MINGW_ARCH}.zip
---
+++ mingw-cross/x86_64/run.bat
@@ -0,0 +1,1 @@
+bin\quake2world.exe
