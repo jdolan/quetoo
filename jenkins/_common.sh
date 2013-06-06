aa4e6131b04af6f0e847b99812b76ad3bb3870ab
Marcel Wysocki <maci@satgnu.net>
2013-06-06 15:42:10 +0200

    another oopsie

--- jenkins/_common.sh
+++ jenkins/_common.sh
@@ -9,7 +9,7 @@ if ([ "${CHROOT}" == "mingw64" ] || [ "${CHROOT}" == "mingw32" ])
 then
 	MINGW_TARGET=${CHROOT}
 	CHROOT="fedora-18-x86_64"
-	DEPS="openssh-clients rsync ${MINGW_TARGET}-SDL ${MINGW_TARGET}-SDL_image ${MINGW_TARGET}-SDL_mixer ${MINGW_TARGET}-curl ${MINGW_TARGET}-physfs ${MINGW_TARGET}-glib2 ${MINGW_TARGET}-libjpeg-turbo libtool ${MINGW_TARGET}-zlib ${MINGW_TARGET}-pkg-config ${MINGW_TARGET}-pdcurses http://maci.satgnu.net/rpmbuild/RPMS/${MINGW_TARGET}-physfs-2.0.3-3.fc18.noarch.rpm http://maci.satgnu.net/rpmbuild/RPMS/${MINGW_TARGET}lzma-sdk457-4.57-2.fc18.noarch.rpm"
+	DEPS="openssh-clients rsync ${MINGW_TARGET}-SDL ${MINGW_TARGET}-SDL_image ${MINGW_TARGET}-SDL_mixer ${MINGW_TARGET}-curl ${MINGW_TARGET}-physfs ${MINGW_TARGET}-glib2 ${MINGW_TARGET}-libjpeg-turbo libtool ${MINGW_TARGET}-zlib ${MINGW_TARGET}-pkg-config ${MINGW_TARGET}-pdcurses http://maci.satgnu.net/rpmbuild/RPMS/${MINGW_TARGET}-physfs-2.0.3-3.fc18.noarch.rpm http://maci.satgnu.net/rpmbuild/RPMS/${MINGW_TARGET}-lzma-sdk457-4.57-2.fc18.noarch.rpm"
 else
 	DEPS="openssh-clients rsync SDL-devel SDL_image-devel SDL_mixer-devel curl-devel physfs-devel glib2-devel libjpeg-turbo-devel libtool zlib-devel ncurses-devel check check-devel"
 fi
