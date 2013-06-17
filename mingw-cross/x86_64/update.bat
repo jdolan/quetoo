6f4a3b52358bd192fca95a1878ae0ca910151ff5
Marcel Wysocki <maci@satgnu.net>
2013-06-17 11:14:57 +0200

    hopefully fix windows rsync permission problems

--- mingw-cross/x86_64/update.bat
+++ mingw-cross/x86_64/update.bat
@@ -1,1 +1,1 @@
-bin\rsync.exe --delete -rkzhP rsync://jdolan.dyndns.org/quake2world-win32/x86_64/ .
+bin\rsync.exe --delete -rkzhP -Z --no-p --no-g --chmod=ugo=rwX rsync://jdolan.dyndns.org/quake2world-win32/x86_64/ .
