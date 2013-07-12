32309d2c6cbbc30c34b281723134c4b088c9d571
jdolan <jay@jaydolan.com>
2013-07-12 09:19:51 -0400

    Update.bat should point to quake2world.net, not jdolan.dyndns.org.

--- mingw-cross/i686/update.bat
+++ mingw-cross/i686/update.bat
@@ -1,1 +1,1 @@
-bin\rsync.exe --delete -rkzhP --perms --chmod=a=rwx,Da+x rsync://jdolan.dyndns.org/quake2world-win32/i686/ .
+bin\rsync.exe --delete -rkzhP --perms --chmod=a=rwx,Da+x rsync://quake2world.net/quake2world-win32/i686/ .
