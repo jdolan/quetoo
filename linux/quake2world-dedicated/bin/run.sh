dd58dcab2eefecfc4e983c3a2052458ccdcdae15
jdolan <jay@jaydolan.com>
2013-07-19 00:24:39 -0400

    Fix LD_LIBRARY_PATH delimiter.

--- linux/quake2world-dedicated/bin/run.sh
+++ linux/quake2world-dedicated/bin/run.sh
@@ -3,7 +3,7 @@
 SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
 QUAKE2WORLD_HOME="${SCRIPT_PATH%/bin*}"
 
-export LD_LIBRARY_PATH="${QUAKE2WORLD_HOME}/lib;${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${QUAKE2WORLD_HOME}/lib:${LD_LIBRARY_PATH}"
 
 ${QUAKE2WORLD_HOME}/bin/quake2world $@
 
