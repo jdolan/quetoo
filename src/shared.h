8676a385a3356381a6e3747691015a98b6f3ddd6
jdolan <jay@jaydolan.com>
2013-11-15 22:25:23 -0500

    Define M_PI for Windows.

--- src/shared.h
+++ src/shared.h
@@ -33,6 +33,10 @@ extern vec3_t vec3_origin;
  * @brief Math library.
  */
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
+
 #define Clamp(x, y, z)			(x < y ? y : x > z ? z : x)
 #define DotProduct(x,y)			(x[0] * y[0] + x[1] * y[1] + x[2] * y[2])
 #define VectorSubtract(a,b,c)	(c[0] = a[0] - b[0], c[1] = a[1] - b[1], c[2] = a[2] - b[2])
