0be81d58375bc56a198448bc178e445ea756aa35
Jay Dolan <jay@jaydolan.com>
2015-02-04 11:59:07 -0500

    Add Vector4Compare.

--- src/shared.h
+++ src/shared.h
@@ -50,6 +50,7 @@ extern vec3_t vec3_forward;
 #define Clamp(x, y, z)				(x < y ? y : x > z ? z : x)
 #define DotProduct(x, y)			(x[0] * y[0] + x[1] * y[1] + x[2] * y[2])
 #define VectorCompare(a, b)			(a[0] == b[0] && a[1] == b[1] && a[2] == b[2])
+#define Vector4Compare(a, b)		(a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3])
 #define VectorAdd(a,b,c)			(c[0] = a[0] + b[0], c[1] = a[1] + b[1], c[2] = a[2] + b[2])
 #define VectorSubtract(a, b, c)		(c[0] = a[0] - b[0], c[1] = a[1] - b[1], c[2] = a[2] - b[2])
 #define VectorScale(a, s, b)		(b[0] = a[0] * (s), b[1] = a[1] * (s), b[2] = a[2] * (s))
