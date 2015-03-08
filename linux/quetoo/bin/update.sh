e652644211318d256326a67bf389b77343b7af22
Marcel Wysocki <maci.stgn@gmail.com>
2015-03-08 03:08:41 +0100

    Minor fixes

--- linux/quetoo/bin/update.sh
+++ linux/quetoo/bin/update.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
-QUETOO="${SCRIPT_PATH%/bin*}"
+QUETOO_HOME="${SCRIPT_PATH%/bin*}"
 
-test -w "${QUETOO}" || {
-	echo "${QUETOO} is not writable" >&2
+test -w "${QUETOO_HOME}" || {
+	echo "${QUETOO_HOME} is not writable" >&2
 	exit 1
 }
 
 ARCH=$(uname -m)
 
 echo
-echo "Updating ${QUETOO} for ${ARCH}.."
+echo "Updating $QUETOO_HOME for ${ARCH}.."
 echo
 
-LINUX=rsync://quetoo.org/quetoo-linux/${ARCH}/
-rsync -rLzhP --delete "${LINUX}" "${QUETOO}" || exit 2
+LINUX=rsync://quetoo.org/quetoo-dedicated-linux/${ARCH}/
+rsync -rLzhP --delete "${LINUX}" "${QUETOO_HOME}" || exit 2
 
 # Ensure that the .desktop entries are in working order
-sed -i "s:%QUETOO%:${QUETOO}:g" \
-	"${QUETOO}/Quetoo.desktop" \
-	"${QUETOO}/Quetoo Update.desktop"
+sed -i "s:%QUETOO_HOME%:${QUETOO_HOME}:g" \
+	"${QUETOO_HOME}/Quetoo.desktop" \
+	"${QUETOO_HOME}/Quetoo Update.desktop"
 
+chmod +x "${QUETOO_HOME}/Quetoo.desktop" \
+	"${QUETOO_HOME}/Quetoo Update.desktop"
 echo
 echo "Update complete."
 echo
