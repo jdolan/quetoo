3e8fa185653fd3a32effe4967afcdd5badae691e
Jay Dolan <jay@jaydolan.com>
2016-01-01 12:07:32 -0500

    Add --skip-compress to Windows updaters.

--- mingw-cross/Quetoo-i686/update.bat
+++ mingw-cross/Quetoo-i686/update.bat
@@ -1,1 +1,1 @@
-bin\rsync.exe --exclude *cygwin* --exclude *rsync* --delete -rkzhP --perms --chmod=a=rwx,Da+x rsync://quetoo.org/quetoo-mingw/i686/ .
+bin\rsync.exe --exclude *cygwin* --exclude *rsync* --delete -rkzhP --perms --chmod=a=rwx,Da+x --skip-compress=pk3 --stats rsync://quetoo.org/quetoo-mingw/i686/ .
