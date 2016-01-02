07b601839a178d68e0b781636306225e38f1c052
Jay Dolan <jay@jaydolan.com>
2016-01-02 00:30:22 -0500

    Nudge game protocol for EF_LIGHT.

--- src/game/default/g_types.h
+++ src/game/default/g_types.h
@@ -28,7 +28,7 @@
  * @brief Game protocol version (protocol minor version). To be incremented
  * whenever the game protocol changes.
  */
-#define PROTOCOL_MINOR 1004
+#define PROTOCOL_MINOR 1005
 
 /*
  * @brief Game-specific server protocol commands. These are parsed directly by
