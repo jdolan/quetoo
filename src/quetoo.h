5ab14e60371fd936c21b0b6ac5c345907aa67965
Jay Dolan <jay@jaydolan.com>
2016-05-07 11:44:42 -0400

    Update the SCREENSHOTS alias to include cl_draw_chat and cl_draw_notify. Extend MAX_TOKEN_CHARS to support long aliases.

--- src/quetoo.h
+++ src/quetoo.h
@@ -80,7 +80,7 @@ typedef dvec_t dvec4_t[4];
  */
 #define MAX_STRING_CHARS	1024 // max length of a string passed to Cmd_TokenizeString
 #define MAX_STRING_TOKENS	128 // max tokens resulting from Cmd_TokenizeString
-#define MAX_TOKEN_CHARS		256 // max length of an individual token
+#define MAX_TOKEN_CHARS		512 // max length of an individual token
 #define MAX_QPATH			64 // max length of a Quake game path
 #define MAX_OS_PATH			260 // max length of a system path
 
