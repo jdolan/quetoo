bec7f2d53e133e368ab0bd77623e702d86cd057d
Jay Dolan <jay@jaydolan.com>
2016-07-18 22:47:31 -0400

    #132 Worlds worst create-server menu.

---
+++ src/client/ui/ui_create_server.c
@@ -0,0 +1,9 @@
+//
+//  ui_create_server.c
+//  quetoo
+//
+//  Created by Jay Dolan on 7/18/16.
+//
+//
+
+#include "ui_create_server.h"
---
+++ src/client/ui/ui_create_server.h
@@ -0,0 +1,14 @@
+//
+//  ui_create_server.h
+//  quetoo
+//
+//  Created by Jay Dolan on 7/18/16.
+//
+//
+
+#ifndef ui_create_server_h
+#define ui_create_server_h
+
+#include <stdio.h>
+
+#endif /* ui_create_server_h */
