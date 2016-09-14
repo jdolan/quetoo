8f01837999e0d3835542d3940ecf72ae8f6bc7fb
Jay Dolan <jay@jaydolan.com>
2016-09-14 11:19:11 -0400

    Fix get_default_dest for mingw.

--- src/tools/update/main.c
+++ src/tools/update/main.c
@@ -63,7 +63,7 @@ static char *get_default_dest(void) {
 			strncpy(dest, exe, len);
 		}
 #elif defined(_WIN32)
-		char *c = strstr(dest, "\\bin\\quetoo.exe");
+		char *c = strstr(dest, "\\bin\\update.exe");
 		if (c) {
 			const size_t len = (c - exe);
 			dest = calloc(len + 1, sizeof(char));
