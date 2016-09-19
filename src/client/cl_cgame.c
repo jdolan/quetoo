89dfeb7a80e84eea8023eda3e29581d00ac9e289
Coşku Baş <cosku.bas@gmail.com>
2016-09-19 19:41:38 +0300

    Fix proposals by jdolan

--- src/client/cl_cgame.c
+++ src/client/cl_cgame.c
@@ -21,8 +21,6 @@
 
 #include "cl_local.h"
 
-#include <SDL2/SDL.h>
-
 static void *cgame_handle;
 
 /**
@@ -128,8 +126,6 @@ void Cl_InitCgame(void) {
 	import.Debug_ = Com_Debug_;
 	import.Warn_ = Com_Warn_;
 	import.Error_ = Cl_CgameError;
-
-	import.Time = SDL_GetTicks;
 
 	import.Malloc = Mem_TagMalloc;
 	import.LinkMalloc = Mem_LinkMalloc;
--- src/console.c
+++ src/console.c
@@ -21,7 +21,6 @@
 
 #include "console.h"
 
-#include <SDL2/SDL.h>
 #include <signal.h>
 
 console_state_t console_state;
@@ -47,7 +46,7 @@ static console_string_t *Con_AllocString(int32_t level, const char *string) {
 	str->size = strlen(str->chars);
 	str->length = StrColorLen(str->chars);
 
-	str->timestamp = SDL_GetTicks();
+	str->timestamp = quetoo.time;
 
 	return str;
 }
