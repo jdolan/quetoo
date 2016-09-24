3244876354c8b0f150fe9dabd5175d2c4e77d64f
Paril <paril@alteredsoftworks.com>
2016-09-24 10:33:29 -0400

    Windows-related files

---
+++ Quetoo.vs15/src/main/winmain.c
@@ -0,0 +1,16 @@
+// Special file to redirect winmain
+#include "config.h"
+#include "client/client.h"
+#include "server/server.h"
+
+extern int32_t main(int32_t argc, char **argv);
+
+int CALLBACK WinMain(
+  _In_ HINSTANCE hInstance,
+  _In_ HINSTANCE hPrevInstance,
+  _In_ LPSTR     lpCmdLine,
+  _In_ int       nCmdShow
+)
+{
+	return main(__argc, __argv);
+}
---
+++ Quetoo.vs15/src/unistd.h
@@ -0,0 +1,5 @@
+#ifdef _WIN64
+typedef __int64 ssize_t;
+#else
+typedef int     ssize_t;
+#endif
