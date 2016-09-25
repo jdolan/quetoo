c1c0c38180eebaa1551ff542f3d86bc4707f626b
Paril <paril@alteredsoftworks.com>
2016-09-25 10:03:04 -0400

    Final cleanup

--- Quetoo.vs15/src/config.h
+++ Quetoo.vs15/src/config.h
@@ -21,10 +21,11 @@
 #define g_list_free_full(list, func)	\
 		g_list_foreach(list, (GFunc)func, NULL); \
 		g_list_free(list);
+#endif
 
+// a fix for glib 2.28
 #define g_hash_table_contains(table, key) \
 		(g_hash_table_lookup(table, key) != NULL)
-#endif
 
 /* Set to the canonical name of the target machine */
 #if defined(_WIN64)
