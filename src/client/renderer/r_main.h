53f3c46276139e61b19663d38c16a20ddfeb620c
Coşku Baş <cosku.bas@gmail.com>
2016-09-26 15:05:47 +0300

    Screenshot fix (#216)
    * Save screenshot as PNG
    
    * Fix doc string
    
    * Fix argument bug
    
    * Remove screenshot quality cvar
    
    * Remove Img_WriteJPG

--- src/client/renderer/r_main.h
+++ src/client/renderer/r_main.h
@@ -51,7 +51,6 @@ extern cvar_t *r_parallax;
 extern cvar_t *r_programs;
 extern cvar_t *r_render_plugin;
 extern cvar_t *r_saturation;
-extern cvar_t *r_screenshot_quality;
 extern cvar_t *r_shadows;
 extern cvar_t *r_shell;
 extern cvar_t *r_specular;
