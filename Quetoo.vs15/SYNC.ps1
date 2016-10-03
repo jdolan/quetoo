3f495171f95cb56999bfaff933e74bd9c49015bb
Paril <paril@alteredsoftworks.com>
2016-10-02 23:45:38 -0400

    Sync fix

--- Quetoo.vs15/SYNC.ps1
+++ Quetoo.vs15/SYNC.ps1
@@ -3,7 +3,7 @@ $QUETOO_PRIVATE_KEY = ${env:QUETOO_PRIVATE_KEY}.Replace("@", "`r`n")
 [System.IO.File]::WriteAllText("quetoo.rsa", $QUETOO_PRIVATE_KEY)
 chmod 0600 quetoo.rsa
 
-echo "Key done, I think? " + [System.IO.File]::Exists("quetoo.rsa").ToString()
+echo "Key done"
 
 $env:Path += ";..\mingw-cross\Quetoo-x86_64\bin\"
 
@@ -23,8 +23,10 @@ $QUETOO_DIST = "$QUETOO_TARGET/Quetoo-BETA-$QUETOO_HOST.zip"
 $QUETOO_HTTP_REPOSITORY = "quetoo.org:/var/www/quetoo.org/files"
 $QUETOO_HTTP_TARGET = "$QUETOO_REMOTE_USER@$QUETOO_HTTP_REPOSITORY"
 
-$QUETOO_ARTIFACT_URL = "https://ci.appveyor.com/api/projects/Paril/quetoo/artifacts/Quetoo-BETA-MSVC.zip?job=Platform%3A+$env:Platform"
+$QUETOO_ARTIFACT_URL = "https://ci.appveyor.com/api/buildjobs/$env:APPVEYOR_JOB_NAME/artifacts/Quetoo-BETA-MSVC.zip"
 $QUETOO_WEB_ARTIFACT = "/var/www/quetoo.org/files/Quetoo-BETA-$QUETOO_HOST.zip"
+
+echo "Artifact: $QUETOO_ARTIFACT_URL"
 
 $PIECE_OF_SHIT = "cd /opt/rsync/quetoo-msvc/$QUETOO_ARCH &&
 wget -O $QUETOO_WEB_ARTIFACT $QUETOO_ARTIFACT_URL &&
