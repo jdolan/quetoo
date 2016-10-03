ad2469dfe9ba43d00c2e9d1368c9ae066eb082d7
Paril <paril@alteredsoftworks.com>
2016-10-03 19:30:25 -0400

    Try with /

--- Quetoo.vs15/SYNC.ps1
+++ Quetoo.vs15/SYNC.ps1
@@ -1,3 +1,3 @@
 $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
 $postParams = @{arch=$QUETOO_ARCH;job_id=$env:APPVEYOR_JOB_ID;secret=$env:QUETOO_SECRET}
-Invoke-WebRequest -Uri http://quetoo.org/quetoo-msvc-webhook -Method POST -Body $postParams
+Invoke-WebRequest -Uri http://quetoo.org/quetoo-msvc-webhook/ -Method POST -Body $postParams
