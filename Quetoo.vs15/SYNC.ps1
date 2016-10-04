2d2f54e83e7f8bc5261dd6b2dc7ee60e68ef97e4
Paril <paril@alteredsoftworks.com>
2016-10-03 23:25:08 -0400

    Pump out full content instead of preview

--- Quetoo.vs15/SYNC.ps1
+++ Quetoo.vs15/SYNC.ps1
@@ -1,3 +1,4 @@
 $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
 $postParams = @{arch=$QUETOO_ARCH;job_id=$env:APPVEYOR_JOB_ID;secret=$env:QUETOO_SECRET}
-Invoke-WebRequest -Uri http://quetoo.org/quetoo-msvc-webhook/ -Method POST -Body $postParams
+$WebResponse = Invoke-WebRequest -Uri http://quetoo.org/quetoo-msvc-webhook/ -Method POST -Body $postParams
+echo $WebResponse.RawContent
