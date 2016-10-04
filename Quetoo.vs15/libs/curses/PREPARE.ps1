452817793895d81a0267066c7bf27a66fd152eef
Paril <paril@alteredsoftworks.com>
2016-10-04 12:59:43 -0400

    Cache dependencies into AppVeyor to save time

--- Quetoo.vs15/libs/curses/PREPARE.ps1
+++ Quetoo.vs15/libs/curses/PREPARE.ps1
@@ -1,5 +1,7 @@
-Write-Output "Downloading curses..."
-(New-Object System.Net.WebClient).DownloadFile("http://downloads.sourceforge.net/project/pdcurses/pdcurses/3.4/pdcurs34.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fpdcurses%2Ffiles%2Fpdcurses%2F&ts=1475253175&use_mirror=heanet", "tmp.zip")
+if (!(Test-Path tmp.zip)) {
+    Write-Output "Downloading curses..."
+    (New-Object System.Net.WebClient).DownloadFile("http://downloads.sourceforge.net/project/pdcurses/pdcurses/3.4/pdcurs34.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fpdcurses%2Ffiles%2Fpdcurses%2F&ts=1475253175&use_mirror=heanet", "tmp.zip")
+}
+
 Write-Output "Extracting curses..."
 7z e tmp.zip -aos "*.h"
-Remove-Item "tmp.*"
--- Quetoo.vs15/libs/glib/include/PREPARE.ps1
+++ Quetoo.vs15/libs/glib/include/PREPARE.ps1
@@ -1,5 +1,8 @@
-Write-Output "Downloading glib..."
-(New-Object System.Net.WebClient).DownloadFile("http://ftp.gnome.org/pub/gnome/sources/glib/2.26/glib-2.26.1.tar.bz2", "tmp.tar.bz2")
+If (!(Test-Path tmp.tar.bz2)) {
+    Write-Output "Downloading glib..."
+    (New-Object System.Net.WebClient).DownloadFile("http://ftp.gnome.org/pub/gnome/sources/glib/2.26/glib-2.26.1.tar.bz2", "tmp.tar.bz2")
+}
+
 Write-Output "Extracting glib..."
 7z x tmp.tar.bz2
 7z e tmp.tar -aos "glib-2.26.1\glib\*.h" -oglib\
@@ -8,4 +11,4 @@ Write-Output "Extracting glib..."
 7z e tmp.tar -aos "glib-2.26.1\glib\glibconfig.h.win32" -oglib\
 Move-Item glib\glib.h glib.h
 Move-Item glib\glibconfig.h.win32 glibconfig.h
-Remove-Item "tmp.*"
+Remove-Item "tmp.tar"
