c913218a057b336a51185dd43e4bc4c20c0234df
Paril <paril@alteredsoftworks.com>
2016-10-09 07:33:02 -0400

    Fixed a bug with lighting trail

--- src/cgame/default/cg_entity_trail.c
+++ src/cgame/default/cg_entity_trail.c
@@ -456,7 +456,7 @@ static void Cg_LightningTrail(cl_entity_t *ent, const vec3_t start, const vec3_t
 			return;
 
 		cgi.ColorFromPalette(12 + (Random() & 3), p->part.color);
-		Vector4Set(p->color_vel, 0.0, 0.0, 0.0, -100.0);
+		Vector4Set(p->color_vel, 0.0, 0.0, 0.0, -100);
 
 		p->part.scale = 8.0;
 		p->part.scroll_s = -8.0;
@@ -464,7 +464,7 @@ static void Cg_LightningTrail(cl_entity_t *ent, const vec3_t start, const vec3_t
 		VectorCopy(pos, p->part.org);
 
 		if (dist <= 48.0) {
-			VectorScale(dir, dist, delta);
+			VectorScale(dir, -dist, delta);
 			offset = 0.0;
 		} else {
 			offset = fabs(2.0 * sin(dist));
