37d3da8fcb209e0cb5f23a418e0069554ca818db
Jay Dolan <jay@jaydolan.com>
2016-10-18 14:07:01 -0400

    Formatting and potential uninitialized variable fix.
    @Paril aside from the brace formatting and scope reduction here, the
    real change is that `symbol` won’t be Mem_Free’d if it’s never
    initialized in the first place.

--- src/sys.c
+++ src/sys.c
@@ -35,9 +35,8 @@
 #define RTLD_NOW 0
 #define dlopen(file_name, mode) LoadLibrary(file_name)
 
-const char *dlerror()
-{
-	static char num_buffer[32];
+const char *dlerror() {
+	char num_buffer[32];
 	const DWORD err = GetLastError();
 
 	itoa(err, num_buffer, 10);
@@ -186,45 +185,38 @@ void *Sys_LoadLibrary(const char *name, void **handle, const char *entry_point, 
 void Sys_Backtrace(void) {
 #if HAVE_EXECINFO
 	void *symbols[MAX_BACKTRACE_SYMBOLS];
-	int32_t i;
 
-	i = backtrace(symbols, MAX_BACKTRACE_SYMBOLS);
+	const int32_t i = backtrace(symbols, MAX_BACKTRACE_SYMBOLS);
 	backtrace_symbols_fd(symbols, i, STDERR_FILENO);
 
 	fflush(stderr);
 #elif defined(_MSC_VER)
-	void *symbols[MAXSHORT];
-    int32_t i;
-    WORD frames;
-    SYMBOL_INFO *symbol;
     HANDLE process = GetCurrentProcess();
 	
 	SymSetOptions(SYMOPT_UNDNAME);
 	
-	if (SymInitialize(process, NULL, TRUE))
-	{
-		frames = CaptureStackBackTrace(0, MAXSHORT, symbols, NULL);
+	if (SymInitialize(process, NULL, TRUE)) {
+		void *symbols[MAXSHORT];
+
+		WORD frames = CaptureStackBackTrace(0, MAXSHORT, symbols, NULL);
     
-		symbol = Mem_Malloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char));
+		SYMBOL_INFO *symbol = Mem_Malloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char));
 		symbol->MaxNameLen = 255;
 		symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
 
-		for (i = 0; i < frames; i++)
-		{
+		for (int32_t i = 0; i < frames; i++) {
 			const char *symbol_name = "unknown symbol";
         
 			SymFromAddr(process, (DWORD64)(symbols[i]), 0, symbol);
 
 			if (symbol->NameLen)
 				symbol_name = symbol->Name;
-			if (symbol->ModBase)
-			{
+			if (symbol->ModBase) {
 				IMAGEHLP_MODULE module;
 				module.SizeOfStruct = sizeof(module);
 				SymGetModuleInfo(process, (DWORD)symbol->ModBase, &module);
 				fprintf(stderr, "%s ", module.ImageName);
-			}
-			else
+			} else
 				fprintf(stderr, "unknown module ");
 		
 			fprintf(stderr, "(%s+%lux) [0x%" PRIx64 "]\n", symbol_name, symbol->Register, symbol->Address);
@@ -232,9 +224,7 @@ void Sys_Backtrace(void) {
 
 		fflush(stderr);
 		Mem_Free(symbol);
-	}
-	else
-	{
+	} else {
 		fprintf(stderr, "Couldn't get stack trace.\n");
 		fflush(stderr);
 	}
