5dcad3aa9ad9b5e35790fc199008d660d2e74c3d
Jay Dolan <jay@jaydolan.com>
2016-10-22 11:03:21 -0400

    MOAR COFFEE

--- src/client/renderer/r_image.c
+++ src/client/renderer/r_image.c
@@ -233,6 +233,8 @@ void R_UploadImage(r_image_t *image, GLenum format, byte *data) {
 	glTexImage2D(GL_TEXTURE_2D, 0, format, image->width, image->height, 0, format,
 			GL_UNSIGNED_BYTE, data);
 
+	R_RegisterMedia((r_media_t *) image);
+
 	R_GetError(image->media.name);
 }
 
