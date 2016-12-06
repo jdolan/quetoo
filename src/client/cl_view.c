930525e644925562d079f3a5d5ad6186c8223af3
Jay Dolan <jay@jaydolan.com>
2016-12-06 08:07:45 -0500

    Pass cl.ticks to r_view.time. This allows flares and things to use scaled time.

--- src/client/cl_view.c
+++ src/client/cl_view.c
@@ -198,7 +198,7 @@ void Cl_UpdateView(void) {
 	cls.cgame->UpdateView(&cl.frame);
 
 	// set time
-	r_view.time = quetoo.ticks;
+	r_view.time = cl.ticks;
 
 	// set area bits to mark visible leafs
 	r_view.area_bits = cl.frame.area_bits;
