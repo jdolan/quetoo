bd12e42cb1acad8263e4b6e84ef487b69bbd276a
Jay Dolan <jay@jaydolan.com>
2016-12-12 15:30:17 -0500

    Comment cleanup.

--- src/cgame/cgame.h
+++ src/cgame/cgame.h
@@ -58,7 +58,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Prints a formatted debug message the the configured consoles.
-	 *
 	 * @remarks If the `debug` cvar is unset, this function will simply return.
 	 */
 	void (*Debug_)(const char *func, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
@@ -82,14 +81,12 @@ typedef struct cg_import_s {
 
 	/**
 	 * @param tag The tag to associate the managed block with (e.g. MEM_TAG_CGAME_LEVEL).
-	 *
 	 * @return A newly allocated block of managed memory under the given `tag`.
 	 */
 	void *(*Malloc)(size_t size, mem_tag_t tag);
 
 	/**
 	 * @return A newly allocated block of managed memory, linked to `parent`.
-	 *
 	 * @remarks The returned memory will be freed automatically when `parent` is freed.
 	 */
 	void *(*LinkMalloc)(size_t size, void *parent);
@@ -106,7 +103,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Creates and starts a thread for the given function.
-	 *
 	 * @param name The thread name.
 	 * @param run The thread function.
 	 * @param data User data.
@@ -127,29 +123,24 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Opens the specified file for reading.
-	 *
 	 * @param path The file path (e.g. `"maps/torn.bsp"`).
 	 */
 	file_t *(*OpenFile)(const char *path);
 
 	/**
 	 * @brief Seeks to the specified offset.
-	 *
 	 * @param file The file.
 	 * @param offset The offset.
-	 *
 	 * @return True on success, false on error.
 	 */
 	_Bool (*SeekFile)(file_t *file, size_t offset);
 
 	/**
 	 * @brief Reads from the specified file.
-	 *
 	 * @param file The file.
 	 * @param buffer The buffer into which to read.
 	 * @param size The size of the objects to read.
 	 * @param count The count of the objects to read.
-	 *
 	 * @return The number of objects read, or -1 on failure.
 	 */
 	int64_t (*ReadFile)(file_t *file, void *buffer, size_t size, size_t count);
@@ -172,33 +163,27 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Closes the specified file.
-	 *
 	 * @param file The file.
-	 *
 	 * @return True on success, false on error.
 	 */
 	_Bool (*CloseFile)(file_t *file);
 
 	/**
 	 * @brief Loads the file resource at `path` into the buffer pointed to by `buffer`.
-	 *
 	 * @param path The file path (Quake path, e.g. `"pics/ch1"`).
 	 * @param buffer The destination buffer, which is allocated by this function.
-	 *
 	 * @return The size of the file, in bytes, or `-1` on error.
 	 */
 	int64_t (*LoadFile)(const char *path, void **buffer);
 
 	/**
 	 * @brief Frees a buffer previously returned from `LoadFile`.
-	 *
 	 * @param buffer The buffered file contents.
 	 */
 	void (*FreeFile)(void *buffer);
 
 	/**
 	 * @brief Enumerates files matching `pattern`, calling the given function.
-	 *
 	 * @param pattern A Unix glob style pattern.
 	 * @param enumerator The enumerator function.
 	 * @param data User data.
@@ -214,12 +199,10 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Resolves a console variable, creating it if not found.
-	 *
 	 * @param name The variable name.
 	 * @param value The variable value string.
 	 * @param flags The variable flags (e.g. CVAR_ARCHIVE).
 	 * @param desc The variable description for builtin console help.
-	 *
 	 * @return The console variable.
 	 */
 	cvar_t *(*Cvar)(const char *name, const char *value, uint32_t flags, const char *desc);
@@ -236,7 +219,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Resolves a console variable that is expected to be defined by the engine.
-	 *
 	 * @return The predefined console variable.
 	 */
 	cvar_t *(*CvarGet)(const char *name);
@@ -253,12 +235,10 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Registers and returns a console command.
-	 *
 	 * @param name The command name (e.g. `"wave"`).
 	 * @param function The command function.
 	 * @param flags The command flags (e.g. CMD_CGAME).
 	 * @param desc The command description for builtin console help.
-	 *
 	 * @return The console command.
 	 */
 	cmd_t *(*Cmd)(const char *name, CmdExecuteFunc function, uint32_t flags, const char *desc);
@@ -284,10 +264,8 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Resolves the next key after `from` that references `bind`.
-	 *
 	 * @param from The key to search from (SDL_SCANCODE_UNKNOWN to begin).
 	 * @param bind The key binding to search for.
-	 *
 	 * @return The next key bound to `bind`.
 	 */
 	SDL_Scancode (*KeyForBind)(SDL_Scancode from, const char *bind);
@@ -299,7 +277,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Binds or unbinds `key`.
-	 *
 	 * @param key The key to bind or unbind.
 	 * @param bind The binding, or `NULL` to unbind.
 	 */
@@ -317,7 +294,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Initiates the connection sequence to the specified server address.
-	 *
 	 * @param addr The network server address.
 	 */
 	void (*Connect)(const net_addr_t *addr);
@@ -344,44 +320,37 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Reads a character from the last received network message.
-	 *
 	 * @return The character.
 	 */
 	int32_t (*ReadChar)(void);
 
 	/**
 	 * @brief Reads a byte from the last received network message.
-	 *
 	 * @return The byte.
 	 */
 	int32_t (*ReadByte)(void);
 
 	/**
 	 * @brief Reads a short integer from the last received network message.
-	 *
 	 * @return The short integer.
 	 */
 	int32_t (*ReadShort)(void);
 
 	/**
 	 * @brief Reads a long integer from the last received network message.
-	 *
 	 * @return The long integer.
 	 */
 	int32_t (*ReadLong)(void);
 
 	/**
 	 * @brief Reads a NULL-terminated string from the last received network message.
-	 *
 	 * @return The string.
-	 *
 	 * @remarks The returned value is statically allocated. Do not free it.
 	 */
 	char *(*ReadString)(void);
 
 	/**
 	 * @brief Reads a vector (float) from the last received network message.
-	 *
 	 * @return The vector.
 	 */
 	vec_t (*ReadVector)(void);
@@ -422,21 +391,18 @@ typedef struct cg_import_s {
 
 	/**
 	 * @return The contents mask at the specified point.
-	 *
 	 * @remarks This checks the world model and all known solid entities.
 	 */
 	int32_t (*PointContents)(const vec3_t point);
 
 	/**
 	 * @brief Traces from `start` to `end`, clipping to all known solids matching the given `contents` mask.
-	 *
 	 * @param start The trace start point.
 	 * @param end The trace end point.
 	 * @param mins The trace mins, or `NULL` for point trace.
 	 * @param maxs The trace maxs, or `NULL` for point trace.
 	 * @param skip The entity number to skip (typically our own client).
 	 * @param contents Solids matching this mask will clip the returned trace.
-	 *
 	 * @return A trace result.
 	 */
 	cm_trace_t (*Trace)(const vec3_t start, const vec3_t end, const vec3_t mins, const vec3_t maxs, const uint16_t skip,
@@ -445,7 +411,6 @@ typedef struct cg_import_s {
 	/**
 	 * @param p The point to check.
 	 * @param model The model to check within, or `NULL` for the world model.
-	 *
 	 * @return The BSP leaf at the given point `p`, in the given `model`.
 	 */
 	const r_bsp_leaf_t *(*LeafForPoint)(const vec3_t p, const r_bsp_model_t *model);
@@ -466,16 +431,13 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Loads a sound sample by the given name.
-	 *
 	 * @param name The sample name or alias (e.g. `"weapons/bfg/fire"`, `"*players/common/gurp"`).
-	 *
 	 * @return The loaded sample.
 	 */
 	s_sample_t *(*LoadSample)(const char *name);
 
 	/**
 	 * @brief Adds a sound sample to the playback queue.
-	 *
 	 * @param play The play sample.
 	 */
 	void (*AddSample)(const s_play_sample_t *play);
@@ -487,7 +449,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Resolves an RGB floating point color from the specified palette index.
-	 *
 	 * @param c The palette index.
 	 * @param color The RGB floating point color.
 	 */
@@ -495,10 +456,8 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Query if a box is visible on screen.
-	 *
 	 * @param mins The min bounds point.
 	 * @param maxs The max bounds point.
-	 *
 	 * @return Returns true if the specified bounding box is completely culled by the
 	 * view frustum, false otherwise.
 	 */
@@ -506,10 +465,8 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Query if a sphere is visible on screen.
-	 *
 	 * @param point The central point of the sphere.
 	 * @param radius The radius of the sphere.
-	 *
 	 * @return Returns true if the specified sphere is completely culled by the
 	 * view frustum, false otherwise.
 	 */
@@ -517,40 +474,31 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Sets the drawing color.
-	 *
 	 * @param color The RGBA drawing color.
 	 */
 	void (*Color)(const vec4_t color);
 
 	/**
 	 * @brief Loads the image by `name` into the SDL_Surface `surface`.
-	 *
 	 * @param name The image name (e.g. `"pics/ch1"`).
 	 * @param surface The surface pointer to return.
-	 *
 	 * @return True on success, false on error.
 	 */
 	_Bool (*LoadSurface)(const char *name, SDL_Surface **surface);
 
 	/**
 	 * @brief Loads the image with the given name and type.
-	 *
 	 * @param name The image name, e.g. `"pics/health_i"`.
 	 * @param type The image type, e.g. `IT_PIC`.
-	 *
 	 * @return The image.
-	 *
 	 * @remarks This function never returns `NULL`, but instead will return the null texture.
 	 */
 	r_image_t *(*LoadImage)(const char *name, r_image_type_t type);
 
 	/**
 	 * @brief Creates a blank state for an atlas and returns it.
-	 *
 	 * @param name The name to give to the atlas, e.g. `"my_atlas_is_cool"`
-	 *
 	 * @return The atlas that has been created.
-	 *
 	 * @remarks Start with this function, add images to it with AddImageToAtlas, then call StitchAtlas when
 	 * you are ready. You can then query positions with GetAtlasImageFromAtlas.
 	 */
@@ -558,7 +506,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Add an image to the list of images for this atlas.
-	 *
 	 * @param atlas The atlas to add an image to.
 	 * @param image The image to add to the atlas.
 	 */
@@ -566,17 +513,14 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Resolve an atlas image from an atlas and image.
-	 *
 	 * @param atlas The atlas to fetch the stitched image from.
 	 * @param image The original image you wish to query.
-	 *
 	 * @return An r_atlas_image_t which contains the stitched coordinates of that image.
 	 */
 	const r_atlas_image_t *(*GetAtlasImageFromAtlas)(const r_atlas_t *atlas, const r_image_t *image);
 
 	/**
 	 * @brief Compiles the specified atlas.
-	 *
 	 * @param atlas The atlas to stitch together and produce the image for.
 	 */
 	void (*CompileAtlas)(r_atlas_t *atlas);
@@ -584,18 +528,14 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Loads the material with the given diffuse texture name.
-	 *
 	 * @param name The diffuse texture name, e.g. `"models/objects/rocket/skin"`.
-	 *
 	 * @return The material.
 	 */
 	r_material_t *(*LoadMaterial)(const char *diffuse);
 
 	/**
 	 * @brief Loads the model with the given name.
-	 *
 	 * @param name The model name (e.g. `"models/objects/rocket/tris"`).
-	 *
 	 * @return The model.
 	 */
 	r_model_t *(*LoadModel)(const char *name);
@@ -607,34 +547,28 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Adds an entity to the scene for the current frame.
-	 *
 	 * @return The added entity.
 	 */
 	const r_entity_t *(*AddEntity)(const r_entity_t *e);
 
 	/**
 	 * @brief Adds a linked entity to the scene for the current frame.
-	 *
 	 * @param parent The entity to which this entity is to be linked (e.g. a client).
 	 * @param model The linked entity model.
 	 * @param tag_name The MD3 tag name to resolve in the parent for link alignment.
-	 *
 	 * @return The linked entity.
-	 *
 	 * @remarks The parent entity must have been previously returned by `AddEntity`.
 	 */
 	const r_entity_t *(*AddLinkedEntity)(const r_entity_t *parent, const r_model_t *model, const char *tag_name);
 
 	/**
 	 * @brief Sets the model-view-projection matrix for the given entity.
-	 *
 	 * @param e The entity.
 	 */
 	void (*SetMatrixForEntity)(r_entity_t *e);
 
 	/**
 	 * @brief Change the matrix identified by "id" with the values from "matrix".
-	 *
 	 * @param id The matrix ID.
 	 * @param matrix The new matrix.
 	 */
@@ -642,7 +576,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Fetch the matrix identified by "id" and stick it in "matrix".
-	 *
 	 * @param id The matrix ID.
 	 * @param matrix The matrix to store the active matrix in.
 	 */
@@ -705,7 +638,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Draws an image in orthographic projection on the screen.
-	 *
 	 * @param x The x coordinate, in pixels.
 	 * @param y The y coordinate, in pixels.
 	 * @param scale The image scale.
@@ -715,7 +647,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Draws a filled rectangle in orthographic projection on the screen.
-	 *
 	 * @param x The x coordinate, in pixels.
 	 * @param y The y coordinate, in pixels.
 	 * @param w The width, in pixels.
@@ -727,7 +658,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Binds the font by `name`, optionally returning the character width and height.
-	 *
 	 * @param name The font name (e.g. `"small"`).
 	 * @param cw The optional return pointer for the character width.
 	 * @param ch The optional return pointer for the character height.
@@ -741,7 +671,6 @@ typedef struct cg_import_s {
 
 	/**
 	 * @brief Draws the string `s` at the given coordinates.
-	 *
 	 * @param x The x coordinate, in pixels.
 	 * @param y The y coordinate, in pixels.
 	 * @param s The string.
