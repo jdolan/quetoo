50ba2f3801d12d48bd2aec01e5013506ce7cdbcc
Jay Dolan <jay@jaydolan.com>
2016-12-29 18:06:29 -0500

    Fix nades bind now that hand grenades are a legitimate weapon.

--- src/client/cl_binds.h
+++ src/client/cl_binds.h
@@ -34,7 +34,7 @@ static const char *DEFAULT_BINDS =
     "bind 8 use lightning\n"
     "bind 9 use railgun\n"
     "bind 0 use bfg10k\n"
-    "bind g use grenades\n"
+    "bind g use hand grenades\n"
 
     "bind - cl_view_size_down\n"
     "bind = cl_view_size_up\n"
