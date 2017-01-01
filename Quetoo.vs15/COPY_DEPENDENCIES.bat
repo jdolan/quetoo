07385a5cb81a446b87d25de43ca3ebc69c979378
Paril <paril@alteredsoftworks.com>
2016-12-31 19:22:42 -0500

    Copy all dependencies automatically after a build.

--- Quetoo.vs15/COPY_DEPENDENCIES.bat
+++ Quetoo.vs15/COPY_DEPENDENCIES.bat
@@ -6,7 +6,13 @@ IF [%2] == [""] GOTO INVALID
 set quetoo_folder=%1
 set build_platform=%2
 
-copy "bin\%build_name%\quetoo.*" "%quetoo_folder%\bin\*" /y
+copy "libs\gettext\%build_platform%\bin\*.dll" "%quetoo_folder%\bin\*" /y
+copy "libs\glib\%build_platform%\bin\*.dll" "%quetoo_folder%\bin\*" /y
+copy "libs\sdl_mixer\lib\%build_platform%\*.dll" "%quetoo_folder%\bin\*" /y
+
+copy "..\..\ObjectivelyMVC\ObjectivelyMVC.vs15\libs\sdl\lib\%build_platform%\*.dll" "%quetoo_folder%\bin\*" /y
+copy "..\..\ObjectivelyMVC\ObjectivelyMVC.vs15\libs\sdl_image\lib\%build_platform%\*.dll" "%quetoo_folder%\bin\*" /y
+copy "..\..\ObjectivelyMVC\ObjectivelyMVC.vs15\libs\sdl_ttf\lib\%build_platform%\*.dll" "%quetoo_folder%\bin\*" /y
 
 GOTO DONE
 
