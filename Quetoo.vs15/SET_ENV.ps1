ba36c15b39b8636fe743d8d8ea6534ea30219395
Paril <paril@alteredsoftworks.com>
2016-12-31 19:08:17 -0500

    Remove extraneous dependencies that were copied out from the zips Add .user files for debugging. Shouldn't interfere with user data I believe, but need to test a bit more. Working on automated dependency copy.

---
+++ Quetoo.vs15/SET_ENV.ps1
@@ -0,0 +1,19 @@
+Write-Output ("Current QUETOO_PATH: {0}" -f [Environment]::GetEnvironmentVariable('QUETOO_HOME', 'User'))
+$Quetoo_Path = Read-Host -Prompt "Input path to Quetoo (the folder that has bin, share, etc) or leave empty to unset`n"
+
+if ($Quetoo_Path.length -gt 1) {
+	try {
+		$Quetoo_Path = [System.IO.Path]::GetDirectoryName($Quetoo_Path)
+		[Environment]::SetEnvironmentVariable("QUETOO_HOME", $Quetoo_Path, "User")
+		Write-Output ("QUETOO_HOME is now set to {0}" -f $Quetoo_Path)
+	} catch {
+		[Environment]::SetEnvironmentVariable("QUETOO_HOME", $null, "User")
+		Write-Error "Invalid path!"
+	}
+} else {
+	[Environment]::SetEnvironmentVariable("QUETOO_HOME", $null, "User")
+	Write-Output "QUETOO_HOME is now gone."
+}
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
