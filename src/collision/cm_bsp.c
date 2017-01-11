e555322eb24465134989e11c2489df81eebc41c8
Jay Dolan <jay@jaydolan.com>
2017-01-11 13:47:33 -0500

    Assign bsp_lump_id_t to a valid enum value.

--- src/collision/cm_bsp.c
+++ src/collision/cm_bsp.c
@@ -524,7 +524,7 @@ void Bsp_UnloadLump(bsp_file_t *bsp, const bsp_lump_id_t lump_id) {
  */
 void Bsp_UnloadLumps(bsp_file_t *bsp, const bsp_lump_id_t lump_bits) {
 
-	for (bsp_lump_id_t i = 0; i < BSP_TOTAL_LUMPS; i++) {
+	for (bsp_lump_id_t i = BSP_LUMP_ENTITIES; i < BSP_TOTAL_LUMPS; i++) {
 	
 		if (lump_bits & (bsp_lump_id_t) (i << 1)) {
 			Bsp_UnloadLump(bsp, i);
@@ -601,7 +601,7 @@ _Bool Bsp_LoadLump(file_t *file, bsp_file_t *bsp, const bsp_lump_id_t lump_id) {
  */
 _Bool Bsp_LoadLumps(file_t *file, bsp_file_t *bsp, const bsp_lump_id_t lump_bits) {
 
-	for (bsp_lump_id_t i = 0; i < BSP_TOTAL_LUMPS; i++) {
+	for (bsp_lump_id_t i = BSP_LUMP_ENTITIES; i < BSP_TOTAL_LUMPS; i++) {
 	
 		if (lump_bits & (bsp_lump_id_t) (1 << i)) {
 			if (!Bsp_LoadLump(file, bsp, i)) {
