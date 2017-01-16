7ab87f5d337556777b9dc4c81775a05c998ca853
Paril <paril@alteredsoftworks.com>
2017-01-16 15:59:15 -0500

    msvc -> pc-windows

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -2,10 +2,10 @@ $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
 $QUETOO_BUCKET = "s3://quetoo/"
 
 $QUETOO_RELEASE_SRC = "Quetoo/";
-$QUETOO_RELEASE_BUCKET = $QUETOO_BUCKET + $QUETOO_ARCH + "-msvs"
+$QUETOO_RELEASE_BUCKET = $QUETOO_BUCKET + $QUETOO_ARCH + "-pc-window"
 
 $QUETOO_SNAPSHOT_SRC = "../Quetoo-BETA-MSVC.zip"
-$QUETOO_SNAPSHOT_BUCKET = $QUETOO_BUCKET + "snapshots/Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
+$QUETOO_SNAPSHOT_BUCKET = $QUETOO_BUCKET + "snapshots/Quetoo-BETA-" + $QUETOO_ARCH + "-PC-Windows.zip"
 
 $aws_exe = "aws.exe"
 
