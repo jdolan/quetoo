d51666488abae9dcd32bececbb40b36a72134de0
Paril <paril@alteredsoftworks.com>
2017-01-16 14:42:20 -0500

    Use CLI instead of deployments

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -1,8 +1,10 @@
 $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
-$QUETOO_DEPLOY = "Quetoo " + $env:Platform
-$QUETOO_DIST_NAME = "Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
+$QUETOO_SNAPSHOT = "Quetoo\";
+$QUETOO_BUCKET "s3://quetoo/" + $QUETOO_ARCH + "-msvs"
+$QUETOO_DIST_SRC = "..\Quetoo-BETA-MSVC.zip"
+$QUETOO_DIST = $QUETOO_BUCKET + "/Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
 
-Push-AppveyorArtifact "..\Quetoo-BETA-MSVC.zip" -FileName $QUETOO_DIST_NAME -DeploymentName ("Quetoo Dist " + $env:Platform)
+$aws_exe = "aws.exe"
 
-$root = Resolve-Path ".\Quetoo\";
-[IO.Directory]::GetFiles($root.Path, '*.*', 'AllDirectories') | % { Push-AppveyorArtifact $_ -FileName ("Quetoo\" + $_.Substring($root.Path.Length)) -DeploymentName $QUETOO_DEPLOY }
+&$aws_exe s3 sync $QUETOO_SNAPSHOT $QUETOO_BUCKET
+&$aws_exe s3 cp $QUETOO_DIST_SRC $QUETOO_DIST
