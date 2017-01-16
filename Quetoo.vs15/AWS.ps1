ce1ef79f4c93bf730377b8b0c87c194f2688e32a
Paril <paril@alteredsoftworks.com>
2017-01-16 14:11:11 -0500

    More S3 attempts

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -2,7 +2,7 @@ $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
 $QUETOO_DEPLOY = "Quetoo " + $env:Platform
 $QUETOO_DIST_NAME = "Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
 
-Push-AppveyorArtifact "..\Quetoo-BETA-MSVC.zip" -FileName $QUETOO_DIST_NAME -DeploymentName "Quetoo Dist"
+Push-AppveyorArtifact "..\Quetoo-BETA-MSVC.zip" -FileName $QUETOO_DIST_NAME -DeploymentName ("Quetoo Dist " + $env:Platform)
 
 $root = Resolve-Path ".\Quetoo\";
 [IO.Directory]::GetFiles($root.Path, '*.*', 'AllDirectories') | % { Push-AppveyorArtifact $_ -FileName ("Quetoo\" + $_.Substring($root.Path.Length)) -DeploymentName $QUETOO_DEPLOY }
