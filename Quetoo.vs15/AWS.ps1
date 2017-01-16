a28a8e3d19e214dc464092dbd904873c763c4984
Paril <paril@alteredsoftworks.com>
2017-01-16 15:29:25 -0500

    Remove artifacts, since COPY_* does these already Use robocopy instead of copy AWS update to proper dist files Appveyor YML

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -1,10 +1,13 @@
 $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
-$QUETOO_SNAPSHOT = "Quetoo\";
-$QUETOO_BUCKET = "s3://quetoo/" + $QUETOO_ARCH + "-msvs"
+$QUETOO_BUCKET = "s3://quetoo/"
+
+$QUETOO_SNAPSHOT_SRC = "Quetoo\";
+$QUETOO_SNAPSHOT_BUCKET = $QUETOO_BUCKET + $QUETOO_ARCH + "-msvs"
+
 $QUETOO_DIST_SRC = "..\Quetoo-BETA-MSVC.zip"
-$QUETOO_DIST = $QUETOO_BUCKET + "/Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
+$QUETOO_DIST_BUCKET = $QUETOO_BUCKET + "dist/Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
 
 $aws_exe = "aws.exe"
 
-&$aws_exe s3 sync $QUETOO_SNAPSHOT $QUETOO_BUCKET
-&$aws_exe s3 cp $QUETOO_DIST_SRC $QUETOO_DIST
+&$aws_exe s3 sync $QUETOO_SNAPSHOT_SRC $QUETOO_BUCKET_BUCKET
+&$aws_exe s3 cp $QUETOO_DIST_SRC $QUETOO_DIST_BUCKET
--- Quetoo.vs15/COPY_QUETOO-UPDATE.bat
+++ Quetoo.vs15/COPY_QUETOO-UPDATE.bat
@@ -6,7 +6,7 @@ IF [%2] == [""] GOTO INVALID
 set quetoo_folder=%1
 set build_name=%2
 
-copy "bin\%build_name%\quetoo-update*" "%quetoo_folder%\bin\*" /y
+robocopy "bin/%build_name%/" "%quetoo_folder%/bin/" quetoo-update* /E /NJH /NJS /FP /NP /V | findstr /v "*EXTRA File"
 
 GOTO DONE
 
