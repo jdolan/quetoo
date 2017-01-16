7e0c8f1ed9d421d9a98fe8617e32079287514e4c
Paril <paril@alteredsoftworks.com>
2017-01-16 13:23:28 -0500

    AWS appveyor config

---
+++ Quetoo.vs15/AWS.ps1
@@ -0,0 +1,8 @@
+$QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
+$QUETOO_DEPLOY = "Quetoo " + $env:Platform
+$QUETOO_DIST_NAME = "Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
+
+Push-AppveyorArtifact "..\Quetoo-BETA-MSVC.zip" -FileName $QUETOO_DIST_NAME -DeploymentName "Quetoo Dist"
+
+$root = Resolve-Path ".\Quetoo\";
+[IO.Directory]::GetFiles($root.Path, '*.*', 'AllDirectories') | % { Push-AppveyorArtifact $_ -FileName $_.Substring($root.Path.Length + 1) -DeploymentName $QUETOO_DIST_NAME }
