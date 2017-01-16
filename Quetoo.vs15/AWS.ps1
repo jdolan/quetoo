6395203c00a84b225a10536100a1ec40e407649f
Paril <paril@alteredsoftworks.com>
2017-01-16 14:49:21 -0500

    Syntax error

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -1,6 +1,6 @@
 $QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
 $QUETOO_SNAPSHOT = "Quetoo\";
-$QUETOO_BUCKET "s3://quetoo/" + $QUETOO_ARCH + "-msvs"
+$QUETOO_BUCKET = "s3://quetoo/" + $QUETOO_ARCH + "-msvs"
 $QUETOO_DIST_SRC = "..\Quetoo-BETA-MSVC.zip"
 $QUETOO_DIST = $QUETOO_BUCKET + "/Quetoo-BETA-" + $QUETOO_ARCH + "-MSVS.zip"
 
