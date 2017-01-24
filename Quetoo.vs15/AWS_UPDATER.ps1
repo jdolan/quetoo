f41c1e5d1ca6ce677bb1cae2038a1ca26d98d80e
Paril <paril@alteredsoftworks.com>
2017-01-24 09:10:30 -0500

    Fix updater I think

--- Quetoo.vs15/AWS_UPDATER.ps1
+++ Quetoo.vs15/AWS_UPDATER.ps1
@@ -5,4 +5,6 @@ $QUETOO_LIB_DIR = $QUETOO_RELEASE_SRC + "lib/"
 
 $aws_exe = "aws.exe"
 
-&$aws_exe s3 cp $QUETOO_BUCKET/snapshots/$QUETOO_UPDATE $QUETOO_LIB_DIR
+$QUETOO_UPDATER_JAR = $QUETOO_BUCKET + "/snapshots/" + $QUETOO_UPDATE
+
+&$aws_exe s3 cp $QUETOO_UPDATER_JAR $QUETOO_LIB_DIR
