056e744fea678157f3c338252f22a15f928a9012
Paril <paril@alteredsoftworks.com>
2017-01-29 12:49:36 -0500

    Use ShellExecuteEx instead since it's the newer/better version Add a few functions to help escape any strings that may need escaping, since on Windows we can't pass an array of strings but rather have to condense it into one string. Annoying to have so much code for such a small thing.

--- src/tools/quetoo-update/main.c
+++ src/tools/quetoo-update/main.c
@@ -33,20 +33,108 @@
 	#include <shellapi.h>
 
 /**
+ * @brief Find the first character of a given list of characters.
+ */
+static int str_find_first_of(const char *string, const char *characters) {
+	
+	for (const char *c = string; *c; c++) {
+		
+		for (const char *character = characters; *character; character++) {
+
+			if (*c == *character) {
+				return string - c;
+			}
+		}
+	}
+
+	return -1;
+}
+
+/**
+ * @brief This routine appends the given argument to a command line such
+    that CommandLineToArgvW will return the argument string unchanged.
+    Arguments in a command line should be separated by spaces; this
+    function does not add these spaces. Thanks @ https://blogs.msdn.microsoft.com/twistylittlepassagesallalike/2011/04/23/everyone-quotes-command-line-arguments-the-wrong-way/
+ */
+static gchar *ArgvQuote(const gchar *argument, const _Bool force)
+{
+    if (!force &&
+        strlen(argument) > 0 &&
+        str_find_first_of(argument, " \t\n\v\"") == -1) {
+		return g_strdup(argument);
+	}
+
+	GString *commandLine = g_string_new("\"");
+
+	for (const char *c = argument; ; ++c) {
+		size_t num_slashes = 0;
+        
+		while (*c && *c == '\\') {
+			c++;
+			num_slashes++;
+		}
+        
+		if (!*c) {
+
+			for (size_t i = 0; i < num_slashes * 2; i++) {
+				g_string_append_c(commandLine, '\\');
+			}
+
+			break;
+		}
+		else if (*c == '"') {
+
+			for (size_t i = 0; i < (num_slashes * 2) + 1; i++) {
+				g_string_append_c(commandLine, '\\');
+			}
+		}
+		else {
+
+			for (size_t i = 0; i < num_slashes; i++) {
+				g_string_append_c(commandLine, '\\');
+			}
+		}
+
+		g_string_append_c(commandLine, *c);
+	}
+    
+	g_string_append_c(commandLine, '"');
+	return g_string_free(commandLine, false);
+}
+
+/**
  * @brief Windows must use CreateProcess because _spawn/_exec on Windows keeps the calling process 
  * running, which is definitely not what we want.
  */
 static intptr_t execvp(const char *filename, const char *const *args) {
-	gchar *cmdline = g_strjoinv(" ", (gchar **) (args + 1));
-	
-	const int result = (int) ShellExecute(NULL, "open", filename, cmdline, NULL, SW_HIDE);
+	GString *cmdline = g_string_new("");
 
-	if (result <= 32) {
-		g_free(cmdline);
+	for (int i = 1; ; i++) {
+		
+		if (!args[i]) {
+			break;
+		} else if (i != 1) {
+			g_string_append_c(cmdline, ' ');
+		}
+
+		gchar *arg = ArgvQuote(args[i], false);
+		g_string_append(cmdline, arg);
+		g_free(arg);
+	}
+
+	SHELLEXECUTEINFO execInfo;
+	memset(&execInfo, 0, sizeof(SHELLEXECUTEINFO));
+	execInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+	execInfo.lpFile = filename;
+	execInfo.lpParameters = cmdline->str;
+	execInfo.nShow = SW_HIDE;
+
+	if (!ShellExecuteEx(&execInfo)) {
+		g_string_free(cmdline, true);
 		return GetLastError();
 	}
 
-	g_free(cmdline);
+	g_string_free(cmdline, true);
 	return 0;
 }
 
