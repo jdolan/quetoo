d1835ffad60214ba2de54756ef2da3553c6c6400
Paril <paril@alteredsoftworks.com>
2017-01-29 16:56:05 -0500

    Get rid of rsync stuff from here for good :D

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -4,7 +4,7 @@ $QUETOO_BUCKET = "s3://quetoo/"
 $QUETOO_RELEASE_SRC = "Quetoo/";
 $QUETOO_RELEASE_BUCKET = $QUETOO_BUCKET + $QUETOO_ARCH + "-pc-windows"
 
-$QUETOO_SNAPSHOT_SRC = "../Quetoo-BETA-MSVC.zip"
+$QUETOO_SNAPSHOT_SRC = "../Quetoo.zip"
 $QUETOO_SNAPSHOT_BUCKET = $QUETOO_BUCKET + "snapshots/Quetoo-BETA-" + $QUETOO_ARCH + "-pc-windows.zip"
 
 $aws_exe = "aws.exe"
--- Quetoo.vs15/SYNC.ps1
+++
@@ -1,4 +0,0 @@
-$QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
-$postParams = @{arch=$QUETOO_ARCH;job_id=$env:APPVEYOR_JOB_ID;secret=$env:QUETOO_SECRET}
-$WebResponse = Invoke-WebRequest -Uri http://quetoo.org/quetoo-msvc-webhook/ -Method POST -Body $postParams
-echo $WebResponse.RawContent
