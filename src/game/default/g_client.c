fa1ec5e0ae953151c9ddaf03ee3baf8ab9ed022f
Paril <paril@alteredsoftworks.com>
2017-02-10 06:31:28 -0500

    Handgernerds

--- src/game/default/g_client.c
+++ src/game/default/g_client.c
@@ -622,6 +622,7 @@ static void G_InitClientInventory(g_entity_t *ent) {
 		G_Give(ent, "Lightning Gun", 200);
 		G_Give(ent, "Hyperblaster", 200);
 		G_Give(ent, "Rocket Launcher", 50);
+		G_Give(ent, "Hand Grenades", 1);
 		G_Give(ent, "Grenade Launcher", 50);
 		G_Give(ent, "Machinegun", 200);
 		G_Give(ent, "Super Shotgun", 80);
