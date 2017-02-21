c8a9d8ef3f1fc651bcab7d32a607f2dea780860f
Paril <paril@alteredsoftworks.com>
2017-02-20 20:43:33 -0500

    Fix autocomplete bug

--- src/console.c
+++ src/console.c
@@ -395,7 +395,9 @@ void Con_WriteHistory(const console_t *console, file_t *file) {
  */
 void Con_AutocompleteInput_f(const uint32_t argi, GList **matches) {
 	const char *partial = Cmd_Argv(argi);
-	const char *pattern = va("%s*", partial);
+	char pattern[strlen(partial) + 2];
+	
+	g_snprintf(pattern, sizeof(pattern), "%s*", partial);
 
 	Cmd_CompleteCommand(pattern, matches);
 	Cvar_CompleteVar(pattern, matches);
