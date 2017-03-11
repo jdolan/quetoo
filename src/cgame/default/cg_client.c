e92a9567d449bf90d806c4b89db03ea00f671b11
KaadmY <kaadmy@gmail.com>
2017-03-10 18:37:44 -0800

    Fix fallback player models/skins

--- src/cgame/default/cg_client.c
+++ src/cgame/default/cg_client.c
@@ -226,8 +226,15 @@ void Cg_LoadClient(cl_client_info_t *ci, const char *s) {
 
 			// load the models
 			if (!Cg_LoadClientModel(ci, info[1], v + 1)) {
-				if (!Cg_LoadClientModel(ci, DEFAULT_CLIENT_MODEL, DEFAULT_CLIENT_SKIN)) {
-					cgi.Error("Failed to load default client model\n");
+				cgi.Verbose("Failed to load client skin %s/%s\n",
+					info[1], v + 1);
+				if (!Cg_LoadClientModel(ci, info[1], DEFAULT_CLIENT_SKIN)) {
+					cgi.Verbose("Failed to load client model %s/%s\n",
+						info[1], DEFAULT_CLIENT_SKIN);
+					if (!Cg_LoadClientModel(ci, DEFAULT_CLIENT_MODEL, DEFAULT_CLIENT_SKIN)) {
+						cgi.Error("Failed to load default client skin %s/%s\n",
+							DEFAULT_CLIENT_MODEL, DEFAULT_CLIENT_SKIN);
+					}
 				}
 			}
 		}
