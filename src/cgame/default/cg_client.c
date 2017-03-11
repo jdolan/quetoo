f909b9d6788f61d0b874ec05e32e1c8cd27e75a8
KaadmY <kaadmy@gmail.com>
2017-03-10 18:37:44 -0800

    Minor cleanup stuff

--- src/cgame/default/cg_client.c
+++ src/cgame/default/cg_client.c
@@ -226,10 +226,14 @@ void Cg_LoadClient(cl_client_info_t *ci, const char *s) {
 
 			// load the models
 			if (!Cg_LoadClientModel(ci, info[1], v + 1)) {
+				cgi.Warn("Failed to load client skin %s/%s\n",
+					info[1], v + 1);
 				if (!Cg_LoadClientModel(ci, info[1], DEFAULT_CLIENT_SKIN)) {
+					cgi.Warn("Failed to load client model %s/%s\n",
+						info[1], DEFAULT_CLIENT_SKIN);
 					if (!Cg_LoadClientModel(ci, DEFAULT_CLIENT_MODEL, DEFAULT_CLIENT_SKIN)) {
-						cgi.Error("Failed to load default client model " \
-							DEFAULT_CLIENT_MODEL "/" DEFAULT_CLIENT_SKIN "\n");
+						cgi.Error("Failed to load default client skin %s/%s\n",
+							DEFAULT_CLIENT_MODEL, DEFAULT_CLIENT_SKIN);
 					}
 				}
 			}
