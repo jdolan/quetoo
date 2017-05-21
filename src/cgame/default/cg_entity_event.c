40f6ab6d8d1716b75d7bdf40fda0783eef467b07
Paril <paril@alteredsoftworks.com>
2017-05-20 21:28:09 -0400

    Random pitch on footsteps

--- src/cgame/default/cg_entity_event.c
+++ src/cgame/default/cg_entity_event.c
@@ -271,6 +271,7 @@ void Cg_EntityEvent(cl_entity_t *ent) {
 			break;
 		case EV_CLIENT_FOOTSTEP:
 			play.sample = Cg_Footstep(ent);
+			play.pitch = (int16_t) (Randomc() * 12.0);
 			break;
 		case EV_CLIENT_GURP:
 			Cg_GurpEffect(ent);
