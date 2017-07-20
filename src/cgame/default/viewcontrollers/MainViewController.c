307597f2a39592769b873df825f6440dfd9abbca
Jay Dolan <jay@jaydolan.com>
2017-07-20 14:57:51 -0400

    Fix #482 in master.

--- src/cgame/default/viewcontrollers/MainViewController.c
+++ src/cgame/default/viewcontrollers/MainViewController.c
@@ -43,7 +43,8 @@ static void action(Control *control, const SDL_Event *event, ident sender, ident
 	NavigationViewController *this = (NavigationViewController *) sender;
 	Class *clazz = (Class *) data;
 	if (clazz) {
-		//$(this, popToRootViewController); FIXME See #482
+		$(this, popToRootViewController);
+		$(this, popViewController);
 
 		ViewController *viewController = $((ViewController *) _alloc(clazz), init);
 
