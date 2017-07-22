7f1e384da1d18b72d927a74cd5e09f1e23e9e666
Jay Dolan <jay@jaydolan.com>
2017-07-22 09:17:49 -0400

    Draggable.

--- src/cgame/default/viewcontrollers/MenuViewController.c
+++ src/cgame/default/viewcontrollers/MenuViewController.c
@@ -62,13 +62,8 @@ static void loadView(ViewController *self) {
 	this->panel = $(alloc(Panel), initWithFrame, NULL);
 	assert(this->panel);
 
-	this->panel->isDraggable = false;
 	this->panel->isResizable = false;
-
-	this->panel->stackView.view.needsLayout = true;
-
 	this->panel->stackView.view.alignment = ViewAlignmentMiddleCenter;
-
 //	this->panel->stackView.view.backgroundColor = QColors.Main;
 //	this->panel->stackView.view.borderColor = QColors.BorderLight;
 
