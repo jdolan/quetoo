bf3cfd71acb537ea61c09d51f2aeeef2e1e2b910
Jay Dolan <jay@jaydolan.com>
2017-09-21 22:37:46 -0400

    Include.

--- src/cgame/default/ui/main/HomeViewController.c
+++ src/cgame/default/ui/main/HomeViewController.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#include "cg_local.h"
+#include <assert.h>
 
 #include "HomeViewController.h"
 #include "Theme.h"
