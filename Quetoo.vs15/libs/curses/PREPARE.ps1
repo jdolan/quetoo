c4922538e5fc62977818d48c66d2625b9203be69
Paril <me@paril.ca>
2018-03-07 17:53:34 -0500

    Include dependencies so Windows users don't suffer Remove prep files Include dlfcn from Objectively (can't dllimport it because the dlfcn library explicitly disallows it for some reason)

--- Quetoo.vs15/libs/curses/PREPARE.ps1
+++
@@ -1,7 +0,0 @@
-if (!(Test-Path tmp.zip)) {
-    Write-Output "Downloading curses..."
-    (New-Object System.Net.WebClient).DownloadFile("https://www.nano-editor.org/dist/win32-support/pdcurs34.zip", "tmp.zip")
-}
-
-Write-Output "Extracting curses..."
-7z e tmp.zip -aos "*.h"
