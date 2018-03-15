1294e685f0cd2dd3f9422805955717cd66edc688
Jay Dolan <jay@jaydolan.com>
2018-03-14 21:49:26 -0400

    Name shader constants after the cvars they expose.

--- src/client/renderer/shaders/include/color.glsl
+++ src/client/renderer/shaders/include/color.glsl
@@ -1,11 +1,10 @@
-#define BRIGHTNESS_SCALE $r_brightness
-#define SATURATION_SCALE $r_saturation
-#define CONTRAST_SCALE $r_contrast
+#define BRIGHTNESS $r_brightness
+#define SATURATION $r_saturation
+#define CONTRAST $r_contrast
 #define MONOCHROME $r_monochrome
 #define INVERT $r_invert
 
-vec4 ColorFilter(in vec4 color)
-{
+vec4 ColorFilter(in vec4 color) {
 #if MONOCHROME
 	color.rgb = vec3((color.r + color.g + color.b) / 3.0);
 #endif
@@ -14,7 +13,7 @@ vec4 ColorFilter(in vec4 color)
 	color.rgb = vec3(1.0) - color.rgb;
 #endif
 
-	color.rgb = mix(vec3(0.5), mix(vec3(dot(vec3(0.2125, 0.7154, 0.0721), color.rgb * BRIGHTNESS_SCALE)), color.rgb * BRIGHTNESS_SCALE, SATURATION_SCALE), CONTRAST_SCALE);
+	color.rgb = mix(vec3(0.5), mix(vec3(dot(vec3(0.2125, 0.7154, 0.0721), color.rgb * BRIGHTNESS)), color.rgb * BRIGHTNESS, SATURATION), CONTRAST);
 
 	return color;
 }
