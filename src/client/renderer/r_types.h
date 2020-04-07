d6c35e23b4ce86fda99861f7607e41fd04c94a4f
Jay Dolan <jay@jaydolan.com>
2020-04-07 11:26:22 -0400

    Pics and atlas need mipmap.

--- src/client/renderer/r_types.h
+++ src/client/renderer/r_types.h
@@ -84,8 +84,8 @@ typedef enum {
 	IT_UI =          (1 <<  3),
 	IT_EFFECT =      (1 <<  4) + (IT_MASK_MIPMAP),
 	IT_MATERIAL =    (1 <<  5) + (IT_MASK_MIPMAP),
-	IT_PIC =         (1 <<  6),
-	IT_ATLAS =       (1 <<  7) + (IT_MASK_CLAMP_EDGE),
+	IT_PIC =         (1 <<  6) + (IT_MASK_MIPMAP),
+	IT_ATLAS =       (1 <<  7) + (IT_MASK_MIPMAP | IT_MASK_CLAMP_EDGE),
 	IT_LIGHTMAP =    (1 <<  8) + (IT_MASK_CLAMP_EDGE),
 	IT_LIGHTGRID =   (1 <<  9) + (IT_MASK_CLAMP_EDGE),
 } r_image_type_t;
