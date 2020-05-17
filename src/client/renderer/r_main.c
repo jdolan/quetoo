3ec9bfd78b6f77b4a86f5a551a120cf74f9d4827
Spiney <pieterverhoevenbe@gmail.com>
2020-05-17 23:39:45 +0200

    Unlatch r_bicubic cvar.

--- src/client/renderer/r_main.c
+++ src/client/renderer/r_main.c
@@ -494,7 +494,7 @@ static void R_InitLocal(void) {
 	r_allow_high_dpi = Cvar_Add("r_allow_high_dpi", "1", CVAR_ARCHIVE | CVAR_R_CONTEXT, "Enables or disables support for High-DPI (Retina, 4K) display modes");
 	r_anisotropy = Cvar_Add("r_anisotropy", "4", CVAR_ARCHIVE | CVAR_R_MEDIA, "Controls anisotropic texture filtering");
 	r_brightness = Cvar_Add("r_brightness", "1", CVAR_ARCHIVE, "Controls texture brightness");
-	r_bicubic = Cvar_Add("r_bicubic", "1", CVAR_ARCHIVE | CVAR_R_MEDIA, "Enable or disable high quality texture filtering on lightmaps and stainmaps.");
+	r_bicubic = Cvar_Add("r_bicubic", "1", CVAR_ARCHIVE, "Enable or disable high quality texture filtering on lightmaps and stainmaps.");
 	r_caustics = Cvar_Add("r_caustics", "1", CVAR_ARCHIVE | CVAR_R_MEDIA, "Enable or disable liquid caustic effects");
 	r_contrast = Cvar_Add("r_contrast", "1", CVAR_ARCHIVE, "Controls texture contrast");
 	r_display = Cvar_Add("r_display", "0", CVAR_ARCHIVE, "Specifies the default display to use");
