3bd38a97ea707280f4e3a0117a4a0de77fd1b4eb
Jay Dolan <jay@jaydolan.com>
2020-11-16 08:38:33 -0500

    Directional vector * matrix should use 0 for w component.

--- src/client/renderer/shaders/mesh_fs.glsl
+++ src/client/renderer/shaders/mesh_fs.glsl
@@ -88,7 +88,7 @@ void main(void) {
 		vec3 diffuse = texture(texture_lightgrid_diffuse, vertex.lightgrid).rgb;
 		vec3 direction = texture(texture_lightgrid_direction, vertex.lightgrid).xyz;
 
-		direction = normalize((view * vec4(direction * 2.0 - 1.0, 1.0)).xyz);
+		direction = normalize((view * vec4(direction * 2.0 - 1.0, 0.0)).xyz);
 
 		vec3 light_diff = diffuse * max(0.0, dot(normal, direction));
 		vec3 light_spec = vec3(0.0);
