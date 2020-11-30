7d73fced6bb60300357230768cdc6dda96099f23
SpineyPete <pieterverhoevenbe@gmail.com>
2020-11-30 21:50:05 +0100

    Update data junction script to make a /share directory if it's not there.

--- Quetoo.vs15/MAKE_DATA_JUNCTION.ps1
+++ Quetoo.vs15/MAKE_DATA_JUNCTION.ps1
@@ -1,6 +1,12 @@
 $Quetoo_Path = [Environment]::GetEnvironmentVariable("QUETOO_HOME", "User")
 
 if ($Quetoo_Path.length -gt 1) {
+    Write-Output "QUETOO_HOME has been set."
+	$share = "{0}\share" -f $Quetoo_Path
+	if (!(Test-Path $share)) {
+        Write "Creating \share directory."
+		New-Item -Path $share -ItemType Directory
+	}
     $path = "{0}\share\default" -f $Quetoo_Path
     cmd /c ('MKLINK /J "{0}" "..\..\quetoo-data\target\default"' -f $path)
 } else {
