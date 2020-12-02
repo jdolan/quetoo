51b8a9d644e932a0880ad02b1d9507471ce1659b
Jay Dolan <jay@jaydolan.com>
2020-12-01 21:13:41 -0500

    Whitespace.

--- src/client/cl_entity.c
+++ src/client/cl_entity.c
@@ -452,10 +452,14 @@ void Cl_Interpolate(void) {
 				ent->maxs = ent->current.maxs;
 			}
 
-			Cm_EntityBounds(ent->current.solid, ent->current.origin, angles, ent->mins, ent->maxs,
-			                &ent->abs_mins, &ent->abs_maxs);
+			Cm_EntityBounds(ent->current.solid, ent->current.origin,
+							angles,
+							ent->mins,
+							ent->maxs,
+			                &ent->abs_mins,
+							&ent->abs_maxs);
 
-			Matrix4x4_CreateFromEntity(&ent->matrix, ent->current.origin, angles, 1.0);
+			Matrix4x4_CreateFromEntity(&ent->matrix, ent->current.origin, angles, 1.f);
 			Matrix4x4_Invert_Simple(&ent->inverse_matrix, &ent->matrix);
 		}
 	}
