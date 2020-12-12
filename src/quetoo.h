f4325d396b67224618eed4b67c49f02352a8856f
Jay Dolan <jay@jaydolan.com>
2020-12-12 10:52:36 -0500

    Revert "Move face contents equality check into FaceCmp."
    This reverts commit baad07fffb2f83106c246164be863585732a9320.

--- src/quetoo.h
+++ src/quetoo.h
@@ -266,12 +266,6 @@ typedef enum {
 #define CONTENTS_MASK_CLIP_PROJECTILE	(CONTENTS_MASK_SOLID | CONTENTS_MASK_MEAT)
 
 /**
- * @brief Faces with differing contents after applying this mask should not be considered equal
- * for face or draw elements merging.
- */
-#define CONTENTS_MASK_FACE_CMP (CONTENTS_MASK_LIQUID)
-
-/**
  * @brief Texinfo flags.
  */
 #define SURF_LIGHT				0x1 // value will hold the light radius
@@ -291,8 +285,7 @@ typedef enum {
 #define SURF_DEBUG_LUXEL		0x10000000 // generate luxel debugging information in quemap
 
 /**
- * @brief Texinfos with differing flags after applying this mask should not be considered
- * equal for face or draw elements merging.
+ * @brief Texinfos with these flags should not be considered equal for draw elements merging.
  */
 #define SURF_MASK_TEXINFO_CMP	~(SURF_LIGHT | SURF_PHONG | SURF_NO_WELD | SURF_DEBUG_LUXEL)
 
@@ -310,11 +303,6 @@ typedef enum {
  * @brief Texinfos with these flags will not have lightmap data.
  */
 #define SURF_MASK_NO_LIGHTMAP	(SURF_SKY | SURF_NO_DRAW | SURF_HINT)
-
-/**
- * @brief Texinfos with these flags will emit no draw elements.
- */
-#define SURF_MASK_NO_DRAW_ELEMENTS SURF_MASK_NO_LIGHTMAP
 
 /**
  * @brief Sound attenuation levels.
