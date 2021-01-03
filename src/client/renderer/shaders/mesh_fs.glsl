79f842a2d06db752f53031cc8fe7dc00e82c9aca
SpineyPete <pieterverhoevenbe@gmail.com>
2021-01-04 00:43:49 +0100

    Dynamic lights broke somehow...

--- src/client/renderer/shaders/mesh_fs.glsl
+++ src/client/renderer/shaders/mesh_fs.glsl
@@ -86,7 +86,7 @@ void main(void) {
 		vec3 light_diffuse = vertex.diffuse * max(0.0, dot(normal, vertex.direction)) + light_ambient;
 		vec3 light_specular = vec3(0.0);
 
-		dynamic_light(vertex.position, normal, 64.0, light_diffuse, light_specular);
+//		dynamic_light(vertex.position, normal, 64.0, light_diffuse, light_specular); // FIXME
 
 		out_color = diffusemap;
 
