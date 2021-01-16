a10004be2315249c61328fdae2dfa42a3d7fabc2
Jay Dolan <jay@jaydolan.com>
2021-01-16 09:46:22 -0500

    Cg_DrawTargetName shouldn't wallhack.

--- src/cgame/default/cg_hud.c
+++ src/cgame/default/cg_hud.c
@@ -1208,7 +1208,7 @@ static void Cg_DrawTargetName(const player_state_t *ps) {
 
 	vec3_t pos = Vec3_Fmaf(cgi.view->origin, MAX_WORLD_DIST, cgi.view->forward);
 
-	const cm_trace_t tr = cgi.Trace(cgi.view->origin, pos, Vec3_Zero(), Vec3_Zero(), 0, CONTENTS_MASK_MEAT);
+	const cm_trace_t tr = cgi.Trace(cgi.view->origin, pos, Vec3_Zero(), Vec3_Zero(), 0, CONTENTS_MASK_CLIP_PROJECTILE);
 	if (tr.fraction < 1.0) {
 
 		const cl_entity_t *ent = &cgi.client->entities[(ptrdiff_t) tr.ent];
