aa00ec94938ba53aaa098a7082b2ead039e61b91
Jay Dolan <jay@jaydolan.com>
2021-01-19 22:34:25 -0500

    Stale SRGB attribute.

--- src/client/renderer/r_context.c
+++ src/client/renderer/r_context.c
@@ -188,8 +188,6 @@ void R_InitContext(void) {
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, s ? 1 : 0);
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, s);
 
-//	SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
-
 	if ((r_context.window = SDL_CreateWindow(PACKAGE_STRING,
 			SDL_WINDOWPOS_CENTERED_DISPLAY(display),
 			SDL_WINDOWPOS_CENTERED_DISPLAY(display), w, h, flags)) == NULL) {
