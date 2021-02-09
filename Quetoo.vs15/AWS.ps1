41443130d7c9f4cc0fe460174e0df483ab9dc77b
Paril <me@paril.ca>
2021-02-09 05:36:04 -0500

    Don't upload full data to /arch/ folder (oops; cancelled in time at least) should be solid now!!

--- Quetoo.vs15/AWS.ps1
+++ Quetoo.vs15/AWS.ps1
@@ -1,21 +1,35 @@
-$QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
+$aws_exe = "aws.exe"
+
+$QUETOO_REVISION_SRC = "revision"
 $QUETOO_BUCKET = "s3://quetoo/"
+$QUETOO_ARCH = If ($env:Platform -Match "Win32") {"i686"} Else {"x86_64"}
+$QUETOO_REVISION_BUCKET = $QUETOO_BUCKET + "revisions/" + $QUETOO_ARCH + "-pc-windows"
+
+# upload revisions/ file which just contains current commit name
+echo "Uploading revisions"
+&$aws_exe s3 cp $QUETOO_REVISION_SRC $QUETOO_REVISION_BUCKET
 
 $QUETOO_RELEASE_SRC = "Quetoo/";
+
 $QUETOO_RELEASE_BUCKET = $QUETOO_BUCKET + $QUETOO_ARCH + "-pc-windows"
-
-$QUETOO_SNAPSHOT_SRC = "Quetoo.zip"
-$QUETOO_SNAPSHOT_BUCKET = $QUETOO_BUCKET + "snapshots/Quetoo-BETA-" + $QUETOO_ARCH + "-pc-windows.zip"
-
-$aws_exe = "aws.exe"
 
 # sync from Quetoo/ to quetoo/<arch>/ bucket
 echo "Syncing to release bucket"
 &$aws_exe s3 sync $QUETOO_RELEASE_SRC $QUETOO_RELEASE_BUCKET --delete
 
+$QUETOO_DATA_BUCKET = "s3://quetoo-data/"
+$QUETOO_DATA_DIR = $QUETOO_RELEASE_SRC + "share/"
+
+# pull in data into Quetoo/share/
+echo "Syncing data"
+&$aws_exe s3 sync $QUETOO_DATA_BUCKET $QUETOO_DATA_DIR
+
+$QUETOO_SNAPSHOT_SRC = "Quetoo.zip"
+$QUETOO_SNAPSHOT_BUCKET = $QUETOO_BUCKET + "snapshots/Quetoo-BETA-" + $QUETOO_ARCH + "-pc-windows.zip"
+
 # zip up Quetoo/
 echo "Zipping snapshot"
-7z a Quetoo.zip Quetoo\
+7z a $QUETOO_SNAPSHOT_SRC Quetoo\
 
 # upload to snapshots/<arch>.zip
 echo "Uploading snapshot"
