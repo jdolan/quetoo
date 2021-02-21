540ac75780101d615909d6394bce97ac693c8350
Paril <me@paril.ca>
2021-02-21 09:13:19 -0500

    Remove bit shifts in glsl, hopefully fixing AMD HD cards

--- src/client/renderer/shaders/uniforms.glsl
+++ src/client/renderer/shaders/uniforms.glsl
@@ -20,9 +20,9 @@
 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#define VIEW_UNKNOWN		(0 << 0)
-#define VIEW_MAIN			(1 << 0)
-#define VIEW_PLAYER_MODEL	(1 << 1)
+#define VIEW_UNKNOWN		0 //(0 << 0)
+#define VIEW_MAIN			1 //(1 << 0)
+#define VIEW_PLAYER_MODEL	2 //(1 << 1)
 
 /**
  * @brief The lightgrid struct.
