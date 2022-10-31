8d8485968aad30aa600888c62af25e69a2336025
Jay Dolan <jay@jaydolan.com>
2022-10-30 20:13:55 -0400

    FFS

--- src/client/renderer/r_mesh_shadow.c
+++ src/client/renderer/r_mesh_shadow.c
@@ -19,38 +19,5 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#pragma once
+#include "r_local.h"
 
-#include "collision/collision.h"
-
-#include "r_gl_types.h"
-
-#include "r_atlas.h"
-#include "r_animation.h"
-#include "r_bloom.h"
-#include "r_bsp.h"
-#include "r_bsp_draw.h"
-#include "r_bsp_model.h"
-#include "r_context.h"
-#include "r_depth_pass.h"
-#include "r_draw_2d.h"
-#include "r_draw_3d.h"
-#include "r_entity.h"
-#include "r_framebuffer.h"
-#include "r_image.h"
-#include "r_light.h"
-#include "r_main.h"
-#include "r_material.h"
-#include "r_media.h"
-#include "r_mesh_draw.h"
-#include "r_mesh_shadow.h"
-#include "r_mesh_model.h"
-#include "r_mesh_model_md3.h"
-#include "r_mesh_model_obj.h"
-#include "r_mesh.h"
-#include "r_model.h"
-#include "r_program.h"
-#include "r_sky.h"
-#include "r_sprite.h"
-#include "r_stain.h"
-#include "r_types.h"
