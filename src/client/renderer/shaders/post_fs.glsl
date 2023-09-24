76b79b83ed24b5a2cdb3329c0fd0da1ee56b3731
Jay Dolan <jay@jaydolan.com>
2023-09-24 18:41:58 -0400

    Let's go with ACES tonemapping.

--- src/client/renderer/shaders/post_fs.glsl
+++ src/client/renderer/shaders/post_fs.glsl
@@ -43,136 +43,17 @@ vec3 color_filter(in vec3 color) {
 	return mix(bias, mix(vec3(dot(luminance, scaled)), scaled, saturation), contrast);
 }
 
-// Narkowicz 2015, "ACES Filmic Tone Mapping Curve"
-vec3 aces(vec3 x) {
-  const float a = 2.51;
-  const float b = 0.03;
-  const float c = 2.43;
-  const float d = 0.59;
-  const float e = 0.14;
-  return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
-}
-
-// Filmic Tonemapping Operators http://filmicworlds.com/blog/filmic-tonemapping-operators/
-vec3 filmic(vec3 x) {
-  vec3 X = max(vec3(0.0), x - 0.004);
-  vec3 result = (X * (6.2 * X + 0.5)) / (X * (6.2 * X + 1.7) + 0.06);
-  return pow(result, vec3(2.2));
-}
-
-// Lottes 2016, "Advanced Techniques and Optimization of HDR Color Pipelines"
-vec3 lottes(vec3 x) {
-  const vec3 a = vec3(1.6);
-  const vec3 d = vec3(0.977);
-  const vec3 hdrMax = vec3(8.0);
-  const vec3 midIn = vec3(0.18);
-  const vec3 midOut = vec3(0.267);
-
-  const vec3 b =
-	  (-pow(midIn, a) + pow(hdrMax, a) * midOut) /
-	  ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
-  const vec3 c =
-	  (pow(hdrMax, a * d) * pow(midIn, a) - pow(hdrMax, a) * pow(midIn, a * d) * midOut) /
-	  ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
-
-  return pow(x, a) / (pow(x, a * d) * b + c);
-}
-
-vec3 reinhard(vec3 x) {
-  return x / (1.0 + x);
-}
-
-vec3 reinhard2(vec3 x) {
-  const float L_white = 4.0;
-
-  return (x * (1.0 + x / (L_white * L_white))) / (1.0 + x);
-}
-
-// Uchimura 2017, "HDR theory and practice"
-// Math: https://www.desmos.com/calculator/gslcdxvipg
-// Source: https://www.slideshare.net/nikuque/hdr-theory-and-practicce-jp
-vec3 uchimura(vec3 x, float P, float a, float m, float l, float c, float b) {
-  float l0 = ((P - m) * l) / a;
-  float L0 = m - m / a;
-  float L1 = m + (1.0 - m) / a;
-  float S0 = m + l0;
-  float S1 = m + a * l0;
-  float C2 = (a * P) / (P - S1);
-  float CP = -C2 / P;
-
-  vec3 w0 = vec3(1.0 - smoothstep(0.0, m, x));
-  vec3 w2 = vec3(step(m + l0, x));
-  vec3 w1 = vec3(1.0 - w0 - w2);
-
-  vec3 T = vec3(m * pow(x / m, vec3(c)) + b);
-  vec3 S = vec3(P - (P - S1) * exp(CP * (x - S0)));
-  vec3 L = vec3(m + a * (x - m));
-
-  return T * w0 + L * w1 + S * w2;
-}
-
-vec3 uchimura(vec3 x) {
-  const float P = 1.0;  // max display brightness
-  const float a = 1.0;  // contrast
-  const float m = 0.22; // linear section start
-  const float l = 0.4;  // linear section length
-  const float c = 1.33; // black
-  const float b = 0.0;  // pedestal
-
-  return uchimura(x, P, a, m, l, c, b);
-}
-
-vec3 uncharted2Tonemap(vec3 x) {
-  float A = 0.15;
-  float B = 0.50;
-  float C = 0.10;
-  float D = 0.20;
-  float E = 0.02;
-  float F = 0.30;
-  float W = 11.2;
-  return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
-}
-
-vec3 uncharted2(vec3 color) {
-  const float W = 11.2;
-  float exposureBias = 2.0;
-  vec3 curr = uncharted2Tonemap(exposureBias * color);
-  vec3 whiteScale = 1.0 / uncharted2Tonemap(vec3(W));
-  return curr * whiteScale;
-}
-
-// Unreal 3, Documentation: "Color Grading"
-// Adapted to be close to Tonemap_ACES, with similar range
-// Gamma 2.2 correction is baked in, don't use with sRGB conversion!
-vec3 unreal(vec3 x) {
-  return x / (x + 0.155) * 1.019;
-}
-
 /**
- * @brief
+ * @brief Narkowicz 2015, "ACES Filmic Tone Mapping Curve"
  */
 vec3 tonemap_color(in vec3 color) {
+	const float a = 2.51;
+	const float b = 0.03;
+	const float c = 2.43;
+	const float d = 0.59;
+	const float e = 0.14;
 
-	switch (tonemap) {
-		case 1:
-			return aces(color);
-		case 2:
-			return filmic(color);
-		case 3:
-			return lottes(color);
-		case 4:
-			return reinhard(color);
-		case 5:
-			return reinhard2(color);
-		case 6:
-			return uchimura(color);
-		case 7:
-			return uncharted2(color);
-		case 8:
-			return unreal(color);
-		default:
-			return color;
-	}
+	return clamp((color * (a * color + b)) / (color * (c * color + d) + e), 0.0, 1.0);
 }
 
 /**
@@ -180,14 +61,15 @@ vec3 tonemap_color(in vec3 color) {
  */
 void main(void) {
 
-	out_color = vec4(0.0);
+	out_color = texture(texture_color_attachment, vertex.texcoord);
 
-	out_color += texture(texture_color_attachment, vertex.texcoord);
 	if (bloom > 0.0) {
 		out_color += texture(texture_bloom_attachment, vertex.texcoord);
 	}
 
 	out_color.rgb = color_filter(out_color.rgb);
 
-	out_color.rgb = tonemap_color(out_color.rgb);
+	if (tonemap > 0.0) {
+		out_color.rgb = tonemap_color(out_color.rgb);
+	}
 }
