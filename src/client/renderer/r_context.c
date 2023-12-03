d4bec343e2065249952c3bb2f527d195c25c263c
Jay Dolan <jay@jaydolan.com>
2023-12-03 10:40:54 -0500

    Request 8 bit default framebuffer channel size.

--- src/client/renderer/r_context.c
+++ src/client/renderer/r_context.c
@@ -266,10 +266,10 @@ void R_InitContext(void) {
 
 	SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
 
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 32);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 32);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 32);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
