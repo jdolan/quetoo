96823a890b254fd3a5b8bbdace66c925cd798c77
Jay Dolan <jay@jaydolan.com>
2024-12-30 17:44:09 -0500

    Change default OQ size.

--- src/client/renderer/r_main.c
+++ src/client/renderer/r_main.c
@@ -366,7 +366,7 @@ static void R_InitLocal(void) {
 	r_error_level = Cvar_Add("r_error_level", "2", CVAR_DEVELOPER, "Error level for more fine-tuned control over KHR_debug reporting. 0 will report all, up to 3 which will only report errors. (developer tool)");
 	r_max_errors = Cvar_Add("r_max_errors", "8", CVAR_DEVELOPER, "The max number of errors before skipping error handlers (developer tool)");
 	r_occlude = Cvar_Add("r_occlude", "1", CVAR_DEVELOPER, "Controls the rendering of occlusion queries (developer tool)");
-	r_occlusion_query_size = Cvar_Add("r_occlusion_query_size", "128", CVAR_DEVELOPER | CVAR_R_MEDIA, "Controls the occlusion query size (developer tool)");
+	r_occlusion_query_size = Cvar_Add("r_occlusion_query_size", "512", CVAR_DEVELOPER | CVAR_R_MEDIA, "Controls the occlusion query size (developer tool)");
 
 	// settings and preferences
 	r_allow_high_dpi = Cvar_Add("r_allow_high_dpi", "1", CVAR_ARCHIVE | CVAR_R_CONTEXT, "Enables or disables support for High-DPI (Retina, 4K) display modes");
