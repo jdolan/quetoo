a7d9ac9aa044f1d9e54a0d4748bcc14c797ac911
Jay Dolan <jay@jaydolan.com>
2025-01-01 20:44:45 -0500

    Fix regression.

--- src/tools/quemap/tree.c
+++ src/tools/quemap/tree.c
@@ -401,7 +401,7 @@ tree_t *BuildTree(csg_brush_t *brushes) {
 	Com_Debug(DEBUG_ALL, "%5i brush sides\n", num_brush_sides);
 
 	tree->head_node = AllocNode();
-	tree->head_node->bounds = tree->bounds;
+	tree->head_node->bounds = Box3f(MAX_WORLD_AXIAL, MAX_WORLD_AXIAL, MAX_WORLD_AXIAL);;
 	tree->head_node->volume = BrushFromBounds(tree->head_node->bounds);
 
 	BuildTree_r(tree->head_node, brushes);
