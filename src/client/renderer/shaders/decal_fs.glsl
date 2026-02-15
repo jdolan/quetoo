dc866693aae920bd9f3dbbb369322b4192d38d35
Jay Dolan <jay@jaydolan.com>
2026-02-14 20:45:21 -0500

    Fog and dynamic lighting on decals.

--- src/client/renderer/shaders/decal_fs.glsl
+++ src/client/renderer/shaders/decal_fs.glsl
@@ -20,13 +20,43 @@
  */
 
 in vertex_data {
+  vec3 position;
   vec2 texcoord;
   vec4 color;
   flat uint time;
   flat uint lifetime;
 } vertex;
 
+uniform mat4 model;
+
 layout (location = 0) out vec4 out_color;
+
+/**
+ * @brief Samples voxel fog
+ */
+vec4 sample_voxel_fog() {
+
+  vec4 fog = vec4(0.0);
+
+  float samples = clamp(length(vertex.position) / BSP_VOXEL_SIZE, 1.0, fog_samples);
+
+  for (float i = 0; i < samples; i++) {
+
+    vec3 xyz = mix((model * vec4(vertex.position, 1.0)).xyz, view[0].xyz, i / samples);
+    vec3 uvw = mix(voxel_uvw(xyz), voxels.view_coordinate.xyz, i / samples);
+
+    fog += texture(texture_voxel_fog, uvw) * vec4(vec3(1.0), fog_density) * min(1.0, samples - i);
+    if (fog.a >= 1.0) {
+      break;
+    }
+  }
+
+  if (hmax(fog.rgb) > 1.0) {
+    fog.rgb /= hmax(fog.rgb);
+  }
+
+  return clamp(fog, 0.0, 1.0);
+}
 
 /**
  * @brief Decal fragment shader.
@@ -36,10 +66,13 @@ void main(void) {
   vec4 diffuse = texture(texture_diffusemap, vertex.texcoord);
 
   out_color = diffuse * vertex.color;
-  
+
   if (vertex.lifetime > 0u) {
     float age = float(uint(ticks) - vertex.time);
     float fade = 1.0 - clamp(age / vertex.lifetime, 0.0, 1.0);
     out_color.a *= fade;
   }
+
+  vec4 fog = sample_voxel_fog();
+  out_color.rgb = mix(out_color.rgb, fog.rgb, fog.a);
 }
--- src/client/renderer/shaders/decal_vs.glsl
+++ src/client/renderer/shaders/decal_vs.glsl
@@ -26,6 +26,7 @@ layout (location = 3) in uint in_time;
 layout (location = 4) in uint in_lifetime;
 
 out vertex_data {
+  vec3 position;
   vec2 texcoord;
   vec4 color;
   flat uint time;
@@ -35,14 +36,60 @@ out vertex_data {
 uniform mat4 model;
 
 /**
+ * @brief
+ */
+vec3 light_and_shadow_light(in int index) {
+
+  light_t light = lights[index];
+
+  float dist = distance(light.origin.xyz, in_position);
+  float radius = light.origin.w;
+  float atten = clamp(1.0 - dist / radius, 0.0, 1.0);
+
+  return light.color.rgb * light.color.a * atten * modulate;
+}
+
+/**
+ * @brief Dynamic lighting for decals
+ */
+void light_and_shadow(void) {
+
+  vec3 diffuse = vec3(0.0);
+
+  ivec3 voxel = voxel_xyz(in_position);
+  ivec2 data = voxel_light_data(voxel);
+
+  for (int i = 0; i < data.y; i++) {
+    int index = voxel_light_index(data.x + i);
+    diffuse += light_and_shadow_light(index);
+  }
+
+  for (int i = 0; i < MAX_DYNAMIC_LIGHTS; i++) {
+    int index = active_lights[i];
+    if (index == -1) {
+      break;
+    }
+
+    diffuse += light_and_shadow_light(index);
+  }
+
+  vertex.color.rgb *= diffuse;
+}
+
+/**
  * @brief Decal vertex shader.
  */
 void main(void) {
 
-  gl_Position = projection3D * view * model * vec4(in_position, 1.0);
+  vec4 position = vec4(in_position, 1.0);
 
+  vertex.position = vec3(view * position);
   vertex.texcoord = in_texcoord;
   vertex.color = in_color;
   vertex.time = in_time;
   vertex.lifetime = in_lifetime;
+
+  light_and_shadow();
+
+  gl_Position = projection3D * view * model * position;
 }
