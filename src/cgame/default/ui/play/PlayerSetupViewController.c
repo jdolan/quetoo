f8fdfbe4b9c1410228cf5af4783786984b212f69
Jay Dolan <jay@jaydolan.com>
2026-03-04 20:01:09 -0500

    Add "hand" Select in PlayerSetupViewController.

--- src/cgame/default/ui/play/PlayerSetupViewController.c
+++ src/cgame/default/ui/play/PlayerSetupViewController.c
@@ -165,10 +165,11 @@ static void loadView(ViewController *self) {
 
   Outlet outlets[] = MakeOutlets(
     MakeOutlet("skin", &this->skinSelect),
-    MakeOutlet("effects", &this->effectsColorPicker),
     MakeOutlet("helmet", &this->helmetColorPicker),
     MakeOutlet("shirt", &this->shirtColorPicker),
     MakeOutlet("pants", &this->pantsColorPicker),
+    MakeOutlet("effects", &this->effectsColorPicker),
+    MakeOutlet("hand", &this->hand),
     MakeOutlet("player", &this->playerModelView)
   );
 
@@ -195,6 +196,10 @@ static void loadView(ViewController *self) {
 
   this->pantsColorPicker->delegate.self = self;
   this->pantsColorPicker->delegate.didPickColor = didPickPlayerColor;
+
+  $(this->hand, addOption, "Center", (ident) 0);
+  $(this->hand, addOption, "Right", (ident) 1);
+  $(this->hand, addOption, "Left", (ident) 2);
 }
 
 /**
